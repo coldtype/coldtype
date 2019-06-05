0446eceaa2393300054ffc652706015ea32f10f9
Rob Stenson <rob.stenson@gmail.com>
2019-06-05 15:28:54 -0700

    a little more style

--- test/test_db.py
+++ test/test_db.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import importlib
 dirname = os.path.dirname(__file__)
 sys.path.insert(0, os.path.realpath(dirname + "/.."))
@@ -11,14 +12,22 @@ from furniture.geometry import Rect
 from random import randint
 
 nostra = "~/Library/Fonts/Nostrav0.9-Stream.otf"
-forma = "~/Library/Fonts/FormaDJRTextRegular.otf"
+forma = "~/Library/Fonts/FormaDJRBannerExtraLightItalic.otf"
 rainer = "~/Library/Fonts/Rainer_v0.2-Bold.otf"
 gig = "~/Library/Fonts/GigV0.2-Regular.otf"
 bild = "~/Library/Fonts/BildVariableV2-VF.ttf"
 
-newPage(1000, 1000)
+in_db = True
+try:
+    newPage(1000, 1000)
+except:
+    from drawBot import *
+    in_db = False
+
 fill(0)
 rect(*Rect.page())
+
+ssv = coldtype.StyledString("VECTORIZED", fontFile=forma, fontSize=200, rect=Rect.page())
 
 ss0 = coldtype.StyledString("Freetype".upper(), fontFile=bild, fontSize=400, rect=Rect.page().offset(0, 100).inset(-20, 0))
 
@@ -26,15 +35,26 @@ ss1 = coldtype.StyledString("Harfbuzz Shaping",
     fontFile="~/Type/fonts/fonts/ObviouslyVariable.ttf",
     fontSize=300,
     tracking=0,
-    features=dict(ss01=True),
-    variations=(dict(wght=1, slnt=1, wdth=1, scale=True)),
+    features=dict(ss01=False),
+    variations=(dict(wght=0.5, slnt=1, wdth=1, scale=True)),
     rect=Rect.page().offset(0, -100))
 
 ss2 = coldtype.StyledString("&", fontFile=nostra, fontSize=500, rect=Rect.page())
 
+with savedState():
+    fill(1, 1, 0)
+    translate(3, 0)
+    ssv.drawBotDraw()
 fill(1)
 ss0.drawBotDraw()
 fill(0, 0.5, 1)
 ss1.drawBotDraw()
 fill(1, 0, 0.5)
 ss2.drawBotDraw()
+
+fill(0)
+ssv.drawBotDraw()
+
+if not in_db:
+    print("Not in drawBot")
+    
