933f4c04369b543a4babec7dd5a6007b32360778
Rob Stenson <rob.stenson@gmail.com>
2020-03-20 12:57:16 -0700

    import Color in drawbotpen

--- coldtype/pens/drawbotpen.py
+++ coldtype/pens/drawbotpen.py
@@ -2,6 +2,7 @@ import drawBot as db
 from coldtype.pens.datpen import DATPen
 from coldtype.geometry import Rect, Edge, Point
 from coldtype.pens.drawablepen import DrawablePenMixin, Gradient
+from coldtype.color import Color
 
 
 def get_image_rect(src):
