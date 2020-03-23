29138daa0cb1fb5bcf0332db28d5cbdc4c23eeaf
Rob Stenson <rob.stenson@gmail.com>
2020-03-23 15:43:10 -0700

    use coldtype-obviously ufo and designspace directly

--- assets/README.md
+++
@@ -1,2 +0,0 @@
-To produce the `variable_ttf/ColdtypeObviously-VF.ttf` artifact, run this fontmake command from within this directory:
-- `fontmake -m ColdtypeObviously.designspace -o variable`
--- examples/example.py
+++ examples/example.py
@@ -4,11 +4,11 @@ from coldtype.text.reader import FontGoggle
 page = Rect(1080, 1080)
 
 #font = FontGoggle("ç/MutatorSans.ttf")
-#font = FontGoggle("≈/Hershey-TriplexGothicEnglish.ufo")
+#font = FontGoggle("ç/ColdtypeObviously_CompressedBlackItalic.ufo")
 font = FontGoggle("ç/ColdtypeObviously.designspace")
 
 def render():
-    style = Style(font, 500, fill="random", wght=1, wdth=1, tu=-50, r=1, ro=1)
+    style = Style(font, 500, fill="random", wdth=1, tu=-50, r=1, ro=1)
     pens = StyledString("COLDTYPE", style).fit(700).pens().align(page).f(None).s("random").sw(2)
     #pens.print_tree()
     return pens.frameSet(), pens
