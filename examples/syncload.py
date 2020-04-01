b0f876a76ff00cf0b4e6aea4ecc2be6bb6ad11f8
Rob Stenson <rob.stenson@gmail.com>
2020-03-31 18:43:51 -0700

    clean up syncload example

--- examples/syncload.py
+++ examples/syncload.py
@@ -1,13 +1,8 @@
-from coldtype import Rect, StyledString, Style, Slug
-from coldtype.text.reader import Font
+from coldtype import *
 
 page = Rect(1080, 1080)
-font = Font("ç/MutatorSans.ttf")
 
+@renderable
 async def render():
     style = Style("ç/MutatorSans.ttf", 250, fill="random", wdth=0, wght=1, tu=-350, r=1, ro=1)
-    pens = StyledString("COLDTYPE", style).pens().align(page).f(0.5, 0, 1).understroke()
-    #pens.print_tree()
-    return pens
-
-renders = [render]
+    return StyledString("COLDTYPE", style).pens().align(page).f(0.5, 0, 1).understroke()
