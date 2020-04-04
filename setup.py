cf390348290a214c0e0c1ba81108895bc68946a0
Rob Stenson <rob.stenson@gmail.com>
2020-04-03 18:25:28 -0700

    add setup.py back

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+long_description = """
+### Coldtype
+"""
+
+setuptools.setup(
+    name="coldtype",
+    version="0.0.1",
+    author="Rob Stenson / Goodhertz",
+    author_email="rob@goodhertz.com",
+    description="Functions for manual vectorized typesetting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goodhertz/coldtype",
+    package_dir="coldtype",
+    packages=[
+        "coldtype",
+        "coldtype.color",
+        "coldtype.pens",
+        "coldtype.renderer",
+        "coldtype.text"
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
