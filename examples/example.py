6f40207d8099cc2a0cbf18d931ef394c41e6441e
Rob Stenson <rob@goodhertz.com>
2020-04-05 16:54:16 -0700

    more bizarre example

--- examples/example.py
+++ examples/example.py
@@ -4,9 +4,9 @@ from coldtype.pens.datpen import warp_fn
 font = Font("ç/ColdtypeObviously.designspace")
 
 @renderable(Rect(1500, 800))
-def render(r):
-    kp = kern_pairs={("T","Y"):-20, ("Y","P"):10,("P","E"):-90}
+def coldtype(r):
+    kp = kern_pairs={("L","D"):-5, ("T","Y"):-20, ("Y","P"):10,("P","E"):-100}
     style = Style(font, 650, fill="random", wdth=1, tu=-50, r=1, ro=1, kp=kp)
     pens = StyledString("COLDTYPE", style).fit(1150).pens().align(r)
-    pens.pmap(lambda idx, p: p.f(complex(0, 0.25+idx/len(pens)*0.5), 0.55, 0.65).flatten(10).nonlinear_transform(warp_fn(mult=35)))
+    pens.pmap(lambda idx, p: p.f(complex(0, 0.25+idx/len(pens)*0.5), 0.55, 0.65).flatten(5).nonlinear_transform(warp_fn(mult=35)))
     return pens.understroke().rotate(5)
