ed9ea1102045265a56c6f319ea3636d35dec28cd
Rob Stenson <rob.stenson@gmail.com>
2020-08-26 10:00:31 -0700

    make test_drawbot_renderable more idiomatic to drawBot

--- test/test_drawbot_renderable.py
+++ test/test_drawbot_renderable.py
@@ -1,12 +1,11 @@
 from coldtype import *
-from random import random
-import drawBot as db
+from drawBot import *
 
-@drawbot_script(svg_preview=0)
+@drawbot_script(svg_preview=1)
 def db_script_test(r):
-    db.fill(1, random(), 0.5)
-    db.oval(500, 500, 1000, 1000)
+    fill(1, random(), 0.5)
+    oval(500, 500, 1000, 1000)
 
-    db.fill(random(), random(), 0.5)
-    db.fontSize(100)
-    db.text("Hallo", (250, 250))
+    fill(random(), random(), 0.5)
+    fontSize(100)
+    text("Hallo", (250, 250))
