c34461fa45135082025de838b29da6477ded1291
Rob Stenson <rob.stenson@gmail.com>
2020-09-08 16:56:07 -0700

    refine the drawbot test animation to show some filmy techniques

--- test/test_drawbot_renderable_animation.py
+++ test/test_drawbot_renderable_animation.py
@@ -11,12 +11,24 @@ tl = Timeline(50, storyboard=[0, 22])
 def db_script_test(f):
     e = f.a.progress(f.i, loops=1, easefn="eeio").e
 
+    def pco(d, tolerance=0.05):
+        x1, y1 = ((1-d)-tolerance, 0)
+        x2, y2 = ((1-d)+tolerance, 1)
+        c = (x1**3 * y2 - x2**3 * y1)/(x1**3 * x2 - x1 * x2**3)
+        a = (y2 - c * x2) / x2**3
+        return [0, c, 0, a]
+
     (StyledString("COLDTYPE",
         Style(co, 150, tu=100+-730*e, ro=1, r=1))
         .pens()
         .align(f.a.r)
         .rotate(15*(1-e))
-        .f(hsl(e, s=0.75, l=0.6))
+        .skew(e*0.5)
+        .filmjitter(e, speed=(20, 30), scale=(3, 4))
+        .f(hsl(e, s=1, l=1-e*0.35))
         .understroke(sw=20)
         .scale(1-1*e*0.5)
-        .db_drawPath(f.a.r, [["gaussianBlur", {"radius":3+20*e}]]))
+        .db_drawPath(f.a.r, [ # TODO autogenerate signatures to make these autocomplete-able?
+            ["gaussianBlur", {"radius":3+20*e}],
+            #["colorPolynomial", {"alphaCoefficients":pco(0.25, 0.1)}]
+        ]))
