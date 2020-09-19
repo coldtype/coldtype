6e1d0349ccc257f605a80109d22304dc23ac2629
Rob Stenson <rob.stenson@gmail.com>
2020-09-19 10:24:31 -0700

    a layered comp to try messing w/ displacement maps

--- test/test_skia.py
+++ test/test_skia.py
@@ -9,33 +9,35 @@ path_effect = skia.PathEffect.MakeSum(
     skia.DiscretePathEffect.Make(10.0, 5.0),
     skia.DiscretePathEffect.Make(10.0, 5.0, randint(0, 1200)))
 
-colorMatrix = [
-    0, 1, 0, 0, 0,
-    1, 1, 0, 0, 0,
-    1, 1, 0, 0.5, 0,
-    0, 0, 0, 0.5, 0
-]
-
 tl = Timeline(60)
 
-@animation(rect=(1200, 550), timeline=tl, storyboard=[0, 15])
+@animation(rect=(1200, 550), timeline=tl, storyboard=[0, 15], bg=0, layers=["__default__", "circle"])
 def render(f):
     r = f.a.r
-    t = tl.progress(f.i, loops=2, easefn="eeio")
-    spp = DATPen().oval(Rect(0, 0, 10, 6)).cast(SkiaPathPen, 6)
+    t = tl.progress(f.i, loops=1, easefn="qeio")
+    spp = DATPen().oval(Rect(0, 0, 5, 5)).cast(SkiaPathPen, 5)
+
+    rd = math.radians(t.e*180)
+    colorMatrix = [
+        math.cos(rd), math.sin(rd), 0, 0, 0,
+        -math.sin(rd), math.cos(rd), 0, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 0, 1, 0
+    ]
+
+    #noise = skia.PerlinNoiseShader.MakeFractalNoise(0.01, 0.01, 1, 5.0)
+    #noise = skia.PerlinNoiseShader.MakeImprovedNoise(0.01, 0.01, 1, t.e)
 
     skp = dict(
         AntiAlias=True,
-        Shader=skia.PerlinNoiseShader.MakeFractalNoise(0.01, 0.01, 1, 5.0),
+        #Shader=noise,
+        #ImageFilter=skia.ImageFilters.Magnifier(skia.Rect(0, 0, t.e*200, 50), 50.0),
         #PathEffect=skia.CornerPathEffect.Make(15.0),
         #PathEffect=skia.DiscretePathEffect.Make(10.0, 4.0),
         #ImageFilter=skia.ImageFilters.DropShadow(5, 5, 10, 10, skia.Color4f(1, 1, 0, 1)),
-        #PathEffect=skia.Path1DPathEffect.Make(spp.path, 20.0, 100.0, skia.Path1DPathEffect.kRotate_Style),
+        #PathEffect=skia.Path1DPathEffect.Make(spp.path, 8.0, t.e*100, skia.Path1DPathEffect.kRotate_Style),
         #ImageFilter=skia.ImageFilters.Erode(5, 12),
-        #ColorFilter=skia.ColorFilters.Matrix(colorMatrix),
-        #Shader=skia.GradientShader.MakeLinear(
-        #    [(0, 0), (1256, 256)],
-        #    [skia.Color(255, 3, 244), skia.Color(15, 17, 258)]),
+        ColorFilter=skia.ColorFilters.Matrix(colorMatrix),
         #MaskFilter=skia.MaskFilter.MakeBlur(skia.kNormal_BlurStyle, 5.0),
         # PathEffect=skia.PathEffect.MakeCompose(
         #    skia.CornerPathEffect.Make(15),
@@ -49,10 +51,21 @@ def render(f):
         .align(r)
         .rotate(-5)
         .translate(0, 10)
-        #.f(Gradient.Horizontal(f.a.r, hsl(0.5, s=0.7), hsl(0.9, s=0.7)))
+        #.f(1)
+        .f(Gradient.Horizontal(f.a.r, hsl(0.5, s=0.7), hsl(0.9, s=0.7)))
         .pmap(lambda i, p: p.flatten(5))
         .attr(skp=skp)
         .understroke(sw=30, s=Gradient.Horizontal(f.a.r, hsl(0.9, s=0.9), hsl(0.5, s=0.9)))
-        )
+        .tag("__default__"))
     
-    return ss
+    circle = (DATPen()
+        .oval(f.a.r.inset(100).square())
+        .translate(-500+t.e*1000, 0)
+        .f(1)
+        .attr(skp=dict(
+            AntiAlias=True,
+            MaskFilter=skia.MaskFilter.MakeBlur(skia.kNormal_BlurStyle, 50.0),
+        ))
+        .tag("circle"))
+    
+    return DATPenSet([ss, circle])
