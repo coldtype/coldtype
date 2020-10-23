cbc155944270c816474d21c620643d0770a27a00
Rob Stenson <rob.stenson@gmail.com>
2020-10-23 14:52:54 -0700

    add test for removeOverlap

---
+++ test/test_varfont.py
@@ -0,0 +1,18 @@
+from coldtype.test import *
+
+vinila = Font.Cacheable("~/Type/fonts/fonts/_wdths/Vinila-VF-HVAR-table.ttf")
+
+@test()
+def varfont(r):
+    dp = (Slug("Boa!",
+            Style(vinila, 340, wdth=0, wght=1, slnt=1, ro=0))
+            .pens()
+            #.removeOverlap()
+            )
+    dps = (DATPenSet([dp])
+        .pen()
+        .removeOverlap()
+        )
+    
+    print(dps)
+    return dps.f(None).s(0).sw(2)
