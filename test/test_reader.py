d94103f00a863a5412f1f9920d0861635ec2d858
Rob Stenson <rob@goodhertz.com>
2020-10-23 20:31:34 -0700

    break external font tests into new test

--- test/test_reader.py
+++ test/test_reader.py
@@ -40,44 +40,7 @@ def test_interp(r):
 
 @test()
 def test_kern_pairs(r):
-    return StyledString("CLD", Style(co, 300, rotate=-10, kp={"C/L":20, "L/D":45})).pens().align(r).f("hr", 0.65, 0.65)
-
-
-@test()
-def test_family_narrowing(r):
-    style = Style("≈/nikolai/Nikolai-Bold.otf", 200, narrower=Style("≈/nikolai/Nikolai-NarrowBold.otf", 200, narrower=Style("≈/nikolai/Nikolai-CondBold.otf", 200)))
-    out = DATPenSet()
-    rs = r.inset(0, 40).subdivide(3, "mxy")
-    out += StyledString("Narrowing", style).pens().align(rs[0])
-    out += StyledString("Narrowing", style).fit(r.w-100).pens().align(rs[1])
-    out += StyledString("Narrowing", style).fit(r.w-200).pens().align(rs[2])
-    return out
-
-
-@test()
-def test_stroke_ufo(r):
-    hershey_gothic = Font("≈/hershey/Hershey-TriplexGothicGerman.ufo")
-    hershey_gothic.load()
-    return StyledString("Grieß".upper(), Style(hershey_gothic, 200, tu=-100)).pens().f(None).s("hr", 0.5, 0.5).sw(3).align(r)
-
-
-@test()
-def test_emoji(r):
-    emoji = StyledString("🍕💽🖥", Style("assets/TwemojiMozilla.ttf", 300, t=20, ch=500, bs=11)).pens().align(r)
-    print(emoji.layered)
-    return emoji
-
-
-@test()
-def test_color_font(r):
-    out = DATPenSet()
-    for x in reversed(range(0, 6)):
-        out += StyledString("COLDTYPE", Style("≈/PappardelleParty-VF.ttf", 550, palette=x)).pens().translate(x*10, x*10)
-    return out.align(r)
-
-
-@test()
-def test_language(r):
-    """Should have an accent over the j"""
-    style = Style("assets/SourceSerifPro-Black.ttf", 350, wdth=1, wght=1, ss01=True)
-    return StyledString("ríjks", style.mod(lang="NLD")).pen().align(r)
+    return (StyledString("CLD",
+        Style(co, 300, rotate=-10, kp={"C/L":20, "L/D":45}))
+        .pens()
+        .align(r).f("hr", 0.65, 0.65))
---
+++ test/test_reader_external_fonts.py
@@ -0,0 +1,40 @@
+from coldtype.test import *
+
+@test()
+def test_family_narrowing(r):
+    style = Style("≈/nikolai/Nikolai-Bold.otf", 200, narrower=Style("≈/nikolai/Nikolai-NarrowBold.otf", 200, narrower=Style("≈/nikolai/Nikolai-CondBold.otf", 200)))
+    out = DATPenSet()
+    rs = r.inset(0, 40).subdivide(3, "mxy")
+    out += StyledString("Narrowing", style).pens().align(rs[0])
+    out += StyledString("Narrowing", style).fit(r.w-100).pens().align(rs[1])
+    out += StyledString("Narrowing", style).fit(r.w-200).pens().align(rs[2])
+    return out
+
+
+@test()
+def test_stroke_ufo(r):
+    hershey_gothic = Font("≈/hershey/Hershey-TriplexGothicGerman.ufo")
+    hershey_gothic.load()
+    return StyledString("Grieß".upper(), Style(hershey_gothic, 200, tu=-100)).pens().f(None).s("hr", 0.5, 0.5).sw(3).align(r)
+
+
+@test()
+def test_emoji(r):
+    emoji = StyledString("🍕💽🖥", Style("assets/TwemojiMozilla.ttf", 300, t=20, ch=500, bs=11)).pens().align(r)
+    print(emoji.layered)
+    return emoji
+
+
+@test()
+def test_color_font(r):
+    out = DATPenSet()
+    for x in reversed(range(0, 6)):
+        out += StyledString("COLDTYPE", Style("≈/PappardelleParty-VF.ttf", 550, palette=x)).pens().translate(x*10, x*10)
+    return out.align(r)
+
+
+@test()
+def test_language(r):
+    """Should have an accent over the j"""
+    style = Style("assets/SourceSerifPro-Black.ttf", 350, wdth=1, wght=1, ss01=True)
+    return StyledString("ríjks", style.mod(lang="NLD")).pen().align(r)
