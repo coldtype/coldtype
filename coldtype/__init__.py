e8dcb29d544213a3adbc807f017ddce755f1fb0b
Rob Stenson <rob.stenson@gmail.com>
2020-10-26 16:59:30 -0700

    bump to 0.1.0

--- coldtype/__init__.py
+++ coldtype/__init__.py
@@ -11,4 +11,4 @@ from coldtype.helpers import *
 from coldtype.animation import *
 
 name = "coldtype"
-__version__ = "0.0.9"
+__version__ = "0.1.0"
--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Rob Stenson, Goodhertz'
 author = 'Rob Stenson, Goodhertz'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.9'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
