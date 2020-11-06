d210e03f29ef4980f77a5b10b79db612f191e6bf
Rob Stenson <rob.stenson@gmail.com>
2020-11-06 09:35:54 -0800

    tmp_angle in test_mouse

--- test/test_mouse.py
+++ test/test_mouse.py
@@ -1,15 +1,22 @@
 from coldtype.test import *
 
+tmp_angle = 0 # "tmp" b/c it will be set back to 0 when the program restarts
+
 @test((1000, 1000), rstate=1)
 def test_oval(r, rs):
+    global tmp_angle
+
     if rs.mouse:
         rm = Rect.FromCenter(rs.mouse, 100)
     else:
         rm = r.take(100, "mdx").square()
     
+    if rs.mods.super:
+        tmp_angle += 33
+    
     return (StyledString("COLDTYPE",
         Style(co, 300, wdth=0))
         .pen()
         .align(rm)
-        .rotate(180 if rs.mods.super else 0)
+        .rotate(tmp_angle)
         .f(hsl(random(), s=0.75)))
