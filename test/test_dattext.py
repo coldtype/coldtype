92ebc57c4df09be189d4d5a3a3b5116988c40fe9
Rob Stenson <rob.stenson@gmail.com>
2020-11-10 08:32:52 -0800

    rename skiatext to dattext test

---
+++ test/test_dattext.py
@@ -0,0 +1,22 @@
+from coldtype.test import *
+
+@test((1000, 300))
+def test_system_font(r):
+    return DATText("Hello, world!", Style("Times", 100, load_font=0, fill=0), r.offset(100, 100))
+
+@test((1000, 300), rstate=1)
+def test_return_string(r, rs):
+    ri = r.inset(30)
+    sx, sy = ri.ipos(rs.mouse, (0, 0))
+
+    rt = r.take(80, "mdy").take(0.8, "mdx")
+    style = Style(co, 150, wdth=sx, fill=(0, 0.5))
+
+    return DATPenSet([
+        (StyledString("COLDTYPE", style)
+            .pens()
+            .align(rt, "mnx")
+            .translate(-5, 17)
+            .f(hsl(0.9))),
+        (DATText("COLDTYPE", style, rt)
+            .translate(0, 0))])
