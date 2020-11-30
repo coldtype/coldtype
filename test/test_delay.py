77c62a0feb93c0278bab99a6059b2e5b7ae8020c
Rob Stenson <rob.stenson@gmail.com>
2020-11-29 21:03:05 -0800

    alternate faster version of test_delay

--- test/test_delay.py
+++ test/test_delay.py
@@ -1,4 +1,5 @@
 from coldtype import *
+from coldtype.warping import warp_fn
 
 co = Font.Cacheable("assets/ColdtypeObviously-VF.ttf")
 
@@ -9,16 +10,17 @@ def var(f, depth=0):
         Style(co, 800-t.e*700, wdth=t.e, ro=1, tu=-90+t.e*50, r=1))
         .pens()
         .align(f.a.r)
-        .f(None)
-        .understroke()
-        .s(1).sw(15-depth*0.65))
+        .f(0)
+        .s(1)
+        .sw(23-depth*0.5)
+        .pmap(lambda i, p: p.nlt(warp_fn(0, 0, mult=30))))
     
-    if depth < 20:
-        cold.insert(0, var.func(Frame((f.i-1)%var.duration, f.a, []), depth=depth+1))
+    if depth < 5:
+        cold.insert(0, var.func(Frame((f.i-2)%var.duration, f.a, []), depth=depth+1))
     
     if depth == 0:
         return DATPenSet([
             DATPen().rect(f.a.r).f(1),
-            cold.phototype(f.a.r, cut=200, cutw=10, fill=bw(0))])
+            cold.phototype(f.a.r, blur=7, cut=200, cutw=3, fill=bw(0))])
     else:
         return cold
