2732b852fc24af280bea711976fc13b428b01d21
Rob Stenson <rob.stenson@gmail.com>
2020-12-03 09:26:58 -0800

    use some different fonts for fun

--- test/test_sonification.py
+++ test/test_sonification.py
@@ -13,7 +13,7 @@ Those waves can then be played back in any DAW and should
 be visible on an x/y scope (like this one http://goodhertz.co/midside-matrix)
 """
 
-#co = Font.Cacheable("~/Type/fonts/fonts/ObviouslyVariable.ttf")
+Style.RegisterShorthandPrefix("≈", "~/Type/fonts/fonts")
 
 class sonification(animation):
     def __init__(self, timeline, filename, samples_per_frame=1, **kwargs):
@@ -52,10 +52,11 @@ class sonification(animation):
             
         obj.close()
 
-def animate_letter(f, l):
+def animate_letter(f, fn):
     e = f.a.progress(f.i, loops=2, easefn="qeio").e
-    c = (StyledString(l,
-        Style(co, 1000-(1-e)*250, wdth=1-e, wght=e))
+    ie = 1 - e
+    txt, style = fn(e, ie)
+    c = (StyledString(txt, style)
         .pens()
         .align(f.a.r)
         .f(None)
@@ -67,16 +68,28 @@ def animate_letter(f, l):
         )
     return c
 
-tl = Timeline(60)
+tl = Timeline(90)
 
 @sonification(tl, "_T.wav")
-def t(f): return animate_letter(f, "T")
+def t(f):
+    return animate_letter(f,
+        lambda e, ie: ("T", Style("≈/ObviouslyVariable.ttf",
+            1000-ie*250, wdth=ie, wght=e)))
 
 @sonification(tl, "_Y.wav")
-def y(f): return animate_letter(f, "Y")
+def y(f):
+    return animate_letter(f,
+        lambda e, ie: ("Y", Style("≈/_wdths/CoFoPeshkaV0.4_Variable.ttf",   
+            1000-e*250, wdth=e, wght=e)))
 
 @sonification(tl, "_P.wav")
-def p(f): return animate_letter(f, "P")
+def p(f):
+    return animate_letter(f,
+        lambda e, ie: ("P", Style("≈/CheeeVariable.ttf",
+            1000, grvt=ie, yest=e)))
 
 @sonification(tl, "_E.wav")
-def e(f): return animate_letter(f, "E")
+def e(f):
+    return animate_letter(f,
+        lambda e, ie: ("E", Style("≈/SwearCilatiVariable.ttf",
+            1000, wght=e)))
