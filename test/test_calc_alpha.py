922d2bc1d22f99aca988568b84a0932f8df1dc3d
Rob Stenson <rob.stenson@gmail.com>
2020-12-22 16:05:03 -0800

    add test for recursively-calculated alpha

---
+++ test/test_calc_alpha.py
@@ -0,0 +1,14 @@
+from coldtype import *
+
+@renderable()
+def nested_alpha(r):
+    dps = DATPenSet([
+        DATPenSet([
+            DATPen().oval(r.inset(50)).f(0).a(0.5)
+        ]).a(0.5),
+    ]).a(0.5)
+
+    dps.walk(lambda p, i, d: None)
+    assert(dps[0][0].calc_alpha() == 0.125)
+
+    return dps
