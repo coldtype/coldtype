e619c5f17d5c720884daeb17ab12e1774126eba0
Rob Stenson <rob.stenson@gmail.com>
2021-01-02 11:53:32 -0800

    add extend to subtitler, move into its own file in nle

--- test/test_lyric_data_entry.py
+++ test/test_lyric_data_entry.py
@@ -1,67 +1,10 @@
 from coldtype.test import *
-from coldtype.animation.sequence import Sequence, ClipTrack, Clip
 from coldtype.animation.audio import Wavfile
-import json
+from coldtype.animation.nle.subtitler import Subtitler
+
 
 fnt = Font.Cacheable("~/Type/fonts/fonts/ObviouslyVariable.ttf")
 audio = Wavfile(__sibling__("media/helloworld.wav"))
-
-class Subtitler(Sequence):
-    def __init__(self, path, duration, fps=30, storyboard=[0]):
-        self.path = Path(path).expanduser().absolute()
-        self.path.parent.mkdir(exist_ok=True, parents=True)
-        try:
-            self.data = json.loads(self.path.read_text())
-        except (FileNotFoundError, json.JSONDecodeError):
-            self.data = dict(tracks=[dict(clips=[])])
-        
-        tracks = []
-        for tidx, t in enumerate(self.data.get("tracks")):
-            clips = []
-            for cidx, c in enumerate(t.get("clips")):
-                clips.append(Clip(c.get("text"), c.get("start"), c.get("end"), cidx, tidx))
-            tracks.append(ClipTrack(self, clips, []))
-        
-        super().__init__(duration, fps, storyboard, tracks)
-    
-    def persist(self):
-        self.conform()
-        self.path.write_text(json.dumps(self.data, indent=2, ensure_ascii=False), encoding="utf-8")
-    
-    def conform(self):
-        clips = self.data["tracks"][self.workarea_track]["clips"]
-        clips = sorted(clips, key=lambda c: c["start"])
-        for cidx, clip in enumerate(clips):
-            try:
-                next_clip = clips[cidx+1]
-            except IndexError:
-                next_clip = None
-            if next_clip:
-                if clip["end"] > next_clip["start"]:
-                    clip["end"] = next_clip["start"]
-    
-    def remove_clip_at_frame(self, fi, tidx):
-        clips = self.data["tracks"][tidx]["clips"]
-        clips = [c for c in clips if c["start"] != fi]
-        return sorted(clips, key=lambda c: c["start"])
-    
-    def overwrite_clips(self, clips, tidx):
-        self.data["tracks"][tidx]["clips"] = clips
-    
-    def delete_clip(self, fi):
-        clips = self.remove_clip_at_frame(fi, self.workarea_track)
-        self.overwrite_clips(clips, self.workarea_track)
-    
-    def add_clip(self, fi, text):
-        clips = self.remove_clip_at_frame(fi, self.workarea_track)
-        end = None
-        for cidx, c in enumerate(clips):
-            if c["start"] > fi:
-                end = c["start"]
-        if end is None:
-            end = self.duration
-        clips.append(dict(text=text, start=fi, end=end))
-        self.overwrite_clips(clips, self.workarea_track)
         
 
 tl = Subtitler("test/lyric_data.json", audio.framelength, storyboard=[3])
@@ -80,7 +23,11 @@ def lyric_entry(f, rs):
         if rs.cmd == "d":
             tl.delete_clip(f.i)
             tl.persist()
-        if rs.cmd == "c":
+        elif rs.cmd == "c":
+            tl.cut_clip(f.i)
+            tl.persist()
+        elif rs.cmd == "e":
+            tl.extend_clip(f.i)
             tl.persist()
 
     return (cg
