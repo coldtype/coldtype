d0743e657d842731e68e82c75ed8f7812d982b87
Rob Stenson <rob.stenson@gmail.com>
2021-01-03 18:50:47 -0800

    add style to lyric test

--- test/test_lyric_data_entry.py
+++ test/test_lyric_data_entry.py
@@ -12,9 +12,11 @@ tl = Subtitler(
 @lyric_editor(tl, bg=0, data_font=recmono)
 def lyric_entry(f, rs):
     def render_clip_fn(f, idx, clip, ftext):
-        return ftext, Style(recmono, 100)
+        if "stylized" in clip.styles:
+            return ftext.upper(), Style(co, 150, wdth=0.5, tu=50, fill=hsl(0.61, s=1, l=0.7))
+        return ftext, Style(recmono, 100, fill=1)
 
-    return (tl.clip_group(0, f.i)
+    return (tl.clip_group(0, f.i, styles=[1])
         .pens(f, render_clip_fn, f.a.r)
         .f(1)
         .xa()
