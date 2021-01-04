a1ee3451c3959aa95f29739d8388156bce2d430c
Rob Stenson <rob.stenson@gmail.com>
2021-01-04 12:34:56 -0800

    minor test edits

--- test/test_composite.py
+++ test/test_composite.py
@@ -15,24 +15,25 @@ def simple(f):
             .f(hsl(le, s=1))
             .s(0).sw(5))]).blendmode(skia.BlendMode.kDifference)
 
-@animation((1200, 800), timeline=Timeline(90, fps=30, jumps=[int(90/4)]), bg=1, composites=1, rstate=1, solo=1)
-def interpolation(f, rs):
+@animation((1200, 800), timeline=Timeline(90, fps=30, jumps=[int(90/4)]), bg=1, composites=1, solo=1)
+def interpolation(f):
     e = f.a.progress(f.i, easefn="qeio", loops=1).e
-    le = f.a.progress(f.i, easefn="linear", loops=1, cyclic=False).e
+    #le = f.a.progress(f.i, easefn="qeio", loops=2, cyclic=True).e
 
-    return DATPenSet([
+    return (DATPenSet([
         DATPen().rect(f.a.r).f(1),
         interpolation.last_result.copy().translate(2, -2) if interpolation.last_result else None,
         (StyledString("C",
-            Style(co, 500-e*300, wdth=1, slnt=e, ro=1, tu=0))
+            Style(co, 500-e*300, wdth=1, ro=1, tu=0))
             .pen()
-            #.mod_contour(1, lambda p: p.rotate(-360*le))
+            #.mod_contour(0, lambda p: p.rotate(-360*le))
             .align(f.a.r, x="mnx")
             .translate(30+790*e, 100)
             #.rotate()
             .f(1)
             .s(0).sw(10))
-            ]).color_phototype(f.a.r, blur=5, cut=135, cutw=15)
+            ])
+        .color_phototype(f.a.r, blur=5, cut=135, cutw=15))
 
 def release(passes):
     (FFMPEGExport(interpolation, passes, loops=4)
