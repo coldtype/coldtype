4457946c323ea1af71fd54fc064b3a60ddba2586
Rob Stenson <rob.stenson@gmail.com>
2021-01-08 17:41:48 -0800

    more minor tweaks

--- test/test_robofont_interaction.py
+++ test/test_robofont_interaction.py
@@ -6,7 +6,7 @@ import json
 datafile = Path("~/robofont-coldtype.json").expanduser()
 tl = Timeline(50)
 
-@animation(timeline=tl, watch_soft=[datafile], rstate=1, bg=1)
+@animation((1500, 1000), timeline=tl, watch_soft=[datafile], rstate=1, bg=1)
 def stub(f, rs):
     r = f.a.r
     nxl = LaunchControlXL(rs.midi)
@@ -26,16 +26,13 @@ def stub(f, rs):
     
     dp_fg = get_l("foreground")
     dp_bg = get_l("narrowester")
-    dp_md = get_l("background")
-
-    #return ,
-    #    f.a.progress(f.i, loops=1, easefn="eeio").e)
+    #dp_md = get_l("background")
 
     return (
-        DATPen.Interpolate([dp_fg, dp_md, dp_bg], f.a.progress(f.i, loops=1, easefn="eeio").e)
+        DATPen.Interpolate([dp_fg, dp_bg], f.a.progress(f.i, loops=1, easefn="ceio").e)
         .f(1)
-        .scale(0.7).translate(-50, 100)
-        #.translate(300, 200)
+        .scale(0.5, center=r.point("SW"))
+        .translate(50, 100)
         .phototype(r,
             blur=1+nxl(10)*30,
             cut=int(nxl(20)*250),
