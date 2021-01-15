62be44e85a7b37fbf92683fb9e8c0f2a5f525b67
Rob Stenson <rob.stenson@gmail.com>
2021-01-15 09:53:32 -0800

    note source of line intersection

--- coldtype/geometry.py
+++ coldtype/geometry.py
@@ -66,6 +66,8 @@ def txt_to_edge(txt):
     else:
         return txt
 
+#https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
+
 def line_intersection(line1, line2):
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
