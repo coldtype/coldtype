5b7f7d96a690a3474df0d9b878f7c7343739b6b1
Rob Stenson <rob@goodhertz.com>
2021-01-31 08:48:10 -0800

    try double-spaced path spec

--- test/test_epl.py
+++ test/test_epl.py
@@ -1,15 +1,26 @@
 from coldtype import *
 
+def dpepl(sdp, s):
+    cmds = s.split("  ")
+    dp = DATPen()
+    for cidx, cmd in enumerate(cmds):
+        v = Rect(0, 0) % sdp.vs(cmd)
+        if cidx == 0:
+            dp.moveTo(v)
+        else:
+            dp.lineTo(v)
+    dp.closePath()
+    return dp
+
 @renderable()
 def epl1(r):
-    #return (StyledString("SZ",
-    #    Style("~/Type/fonts/fonts/OhnoFatfaceVariable.ttf", 500, opsz=0.75, tu=0, wdth=0.25))
-    #    .pen()
-    #    .align(r)
-    #    .f(0))
+    ri = r%"Caƒ100ƒa/@1/Raƒ200ƒa/@1"
+    dp = DP().constants(ri=ri)
+    print(r%dp.vs("$ri/↗"))
     return DPS([
-        DP(r%"Caƒ100ƒa/@1/⊢/OY100"),
-        DP(r%"I100/SY+100/MX-200/—/OY100"),
+        DP(r).f(None).s(0).sw(4),
+        dpepl(dp, "$ri/↗  $ri/↘  $ri/OX-50/←").f(0).s(0),
+        #DP(r%"Caƒ100ƒa/@1/⊢/OY100"),
+        #DP(r%"I100/SY+100/MX-200/—/OY100"),
         #(DP(r.inset(100).subtract(100, "mxy").setmnx(200).ecy.offset(0, 100)).s(0, 0.2).sw(10)),
     ])
-    return None
