e461b984b9925df8dac916f1e47808cac66f89ae
Rob Stenson <rob.stenson@gmail.com>
2021-02-17 16:26:35 -0800

    sort of better test

--- test/test_datimage.py
+++ test/test_datimage.py
@@ -8,5 +8,26 @@ def crop_image(r):
     return DATPens([
         DATImage(src).resize(0.5).align(r).rotate(25).to_pen(r),
         #DATText("Hello", Style("Times", 120, load_font=0), r),
-        DATPen().rect(r).scale(0.85).rotate(25).f(hsl(0.3, a=0.3))
-    ]).print_tree()
+        #DATPen().rect(r).scale(0.85).rotate(25).f(hsl(0.3, a=0.3))
+    ])#.print_tree()
+
+@renderable(solo=0)
+def translate_images(r):
+    return DATPens([
+        DP(r).f(hsl(0.3)),
+        (DATImage(src)
+            .resize(0.5)
+            #.precompose(r)
+            .align(r)
+            .rotate(25)
+            .to_pen(r)
+            .translate(-170, -100)),
+        (DATImage(src)
+            .resize(0.25)
+            #.precompose(r)
+            .align(r)
+            .rotate(-25)
+            #.precompose(r)
+            .to_pen(r)
+            .translate(0, 100))
+    ]).translate(250, 0)
