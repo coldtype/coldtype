d09d78ad9a142c2c397ad6c96dbca1c08e898e71
Rob Stenson <rob.stenson@gmail.com>
2021-02-28 12:15:26 -0800

    use some loop information in the test_single_var animation

--- test/test_single_var.py
+++ test/test_single_var.py
@@ -1,34 +1,36 @@
 from coldtype.test import *
 
-tl = Timeline(60, fps=30)
+tl = Timeline(300, fps=30)
 
-@animation((1600, 600), timeline=tl)
+@animation((1080, 1080), timeline=tl)
 def test_multi_var(f):
-    e = f.a.progress(f.i, loops=1, easefn="eeio").e
-    e2 = f.a.progress(f.i, loops=2, easefn="eei").e
+    e = f.a.progress(f.i, loops=4, easefn="eeio").e
+    e2 = f.a.progress(f.i, loops=5, easefn="eei").e
     fs = 200
+    wght = (1-e)*0.85
 
-    wght = (1-e)*0.25
-
-    pens = (RichText(f.a.r, "T[t] Y[y] P[p] E[e]", {
+    pens = (RichText(f.a.r, "T[t]/Y[y]/P[p]/E[e]", {
             "t": Style(mutator, fs, wdth=e, wght=wght),
             "y": Style(mutator, fs, wdth=1-e2, wght=wght),
             "p": Style(mutator, fs, wdth=e2, wght=wght),
-            "e": Style(mutator, fs, wdth=1-e, wght=wght)})
+            "e": Style(mutator, fs, wdth=1-e, wght=wght)},
+            invisible_boundaries="/")
         .collapse()
         .remove_blanks()
-        .track(40)
         .align(f.a.r)
-        .f(0))
+        .f(1))
 
-    cidx = 2 # the Y
-    return (pens
-        .center_on_point(f.a.r, pens[cidx].bounds().point("C"), interp=e)
-        .scale(1+e*2, point=pens[cidx].bounds().point("C"))
-        .insert(0, DP(f.a.r).f(1)))
+    if True:
+        zoom = f.a.progress(f.i, loops=4, easefn="eeio")
+        cidx = 3 - math.floor(zoom.loop/2)
+        pe = zoom.e
+        (pens.center_on_point(f.a.r, pens[cidx].bounds().point("C"), interp=pe)
+            .scale(1+pe*2, point=pens[cidx].bounds().point("C")))
+
+    return pens.insert(0, DP(f.a.r).f(0))
 
 def release(passes):
-    (FFMPEGExport(test_multi_var, passes, loops=4)
+    (FFMPEGExport(test_multi_var, passes, loops=2)
         .h264()
         .write()
         .open())
