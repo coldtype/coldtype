e0f3cc163914b2436b5db073143f8d3992d4b6bc
Rob Stenson <rob.stenson@gmail.com>
2021-05-10 11:36:42 -0700

    cdelopty restricted idiom

--- coldtype/text/__init__.py
+++ coldtype/text/__init__.py
@@ -10,3 +10,6 @@ import drafting.text.composer
 from drafting.text.reader import Style, StyledString, SegmentedString, normalize_font_path, Font, FontNotFoundException
 from drafting.text.composer import Slug, Lockup, Graf, GrafStyle, T2L, Composer
 #from coldtype.text.richtext import RichText
+
+def StSt(text, font, font_size, **kwargs):
+    return (StyledString(text, Style(font, font_size, **kwargs)))
