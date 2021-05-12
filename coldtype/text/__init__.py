2cfc8456fb4d98bfa81e2d9f280064f34a4b5571
Rob Stenson <rob.stenson@gmail.com>
2021-05-12 09:58:25 -0700

    move StSt to drafting

--- coldtype/text/__init__.py
+++ coldtype/text/__init__.py
@@ -9,19 +9,5 @@ import drafting.text.composer
 
 from drafting.geometry.rect import Rect
 from drafting.text.reader import Style, StyledString, SegmentedString, normalize_font_path, Font, FontNotFoundException
-from drafting.text.composer import Slug, Lockup, Graf, GrafStyle, T2L, Composer
+from drafting.text.composer import Slug, Lockup, Graf, GrafStyle, T2L, Composer, StSt
 #from coldtype.text.richtext import RichText
-
-def StSt(text,
-    font,
-    font_size,
-    rect=Rect(1080, 1080),
-    **kwargs):
-    style = Style(font, font_size, **kwargs)
-    fit = kwargs.get("fit", None)
-    leading = kwargs.get("leading", 10)
-    if "\n" in text:
-        lockup = Composer(rect, text, style, fit=fit, leading=leading)
-    else:
-        lockup = StyledString(text, style)
-    return lockup
