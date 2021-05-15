54fca3788866d8719aedb81aa01bdf100ba2214a
Rob Stenson <rob.stenson@gmail.com>
2021-05-15 10:19:17 -0700

    generative font example

---
+++ examples/generative_font.py
@@ -0,0 +1,148 @@
+from coldtype import *
+from coldtype.pens.dattext import DATText
+
+cap_height = 750
+descender = -250
+font_name = "Generative"
+style_name = "Regular"
+
+# decorator class to wrap glyph-building functions
+
+class glyphfn():
+    def __init__(self, width=1000, lsb=0, rsb=0):
+        """lsb = left-side-bearing / rsb = right-side-bearing"""
+        self.lsb = lsb
+        self.rsb = rsb
+        self.frame = Rect(width, cap_height)
+        self.bbox = Rect(lsb + width + rsb, cap_height)
+    
+    def __call__(self, func):
+        self.func = func
+        self.glyph_name = func.__name__
+        self.unicode = glyph_to_uni(self.glyph_name)
+        return self
+
+# !!! the important part:
+# individual functions to draw glyphs
+# the function names should be canonical
+# "glyphNames" as in assets/glyphNamesToUnicode.txt
+
+@glyphfn(300)
+def space(r):
+    return DP() # i.e. nothing (since it's a blank space)
+
+@glyphfn(500, 10, 10)
+def A(r):
+    return (DATPen()
+        .rect(r)
+        .difference(DP(r.take(100, "mdy")
+            .take(20, "mdx")
+            .offset(0, 100)))
+        .difference(DP(r.take(200, "mny")
+            .take(20, "mdx"))))
+
+@glyphfn(500, 10, 10)
+def B(r):
+    t, b = r.inset(0, 100).subdivide(2, "mxy")
+    return (DATPen()
+        .rect(r)
+        .difference(DP(t.take(100, "mdy")
+            .take(20, "mdx")))
+        .difference(DP(b.take(100, "mdy")
+            .take(20, "mdx")))
+        .difference(DP(r.take(100, "mdy")
+            .take(100, "mxx"))
+            .rotate(45)
+            .translate(50, 0)))
+
+@glyphfn(500, 10, 10)
+def C(r):
+    return (DATPen()
+        .rect(r)
+        .difference(DP(r.take(20, "mdx")
+            .inset(0, 250)))
+        .difference(DP(r.take(0.5, "mxx")
+            .take(20, "mdy"))))
+
+# !!! end of glyph drawing functions
+
+def find_glyph_fns():
+    found = []
+    itms = globals().items()
+    for k, v in itms:
+        if isinstance(v, glyphfn):
+            found.append(v)
+    return found
+
+glyph_fns = find_glyph_fns()
+
+# our useable output is a UFO file,
+# which we should make sure exists
+ufo_path:Path = __sibling__("generative_font.ufo")
+if not ufo_path.exists():
+    ufo = DefconFont()
+    ufo.save(ufo_path)
+
+def show_grid(render, result):
+    if False: # flip to true if you don't want to see the grid
+        return result
+
+    gfn = result[0].data.get("gfn")
+    if not gfn:
+        print("! No glyph found")
+    else:
+        bbox = gfn.bbox.offset(0, 250)
+        return DATPens([
+            DATPens(result).translate(0, 250),
+            DATPen().gridlines(render.rect).s(hsl(0.6, a=0.3)).sw(1).f(None),
+            (DATPen()
+                .line(bbox.es.extr(-100))
+                .line(bbox.en.extr(-100))
+                .line(bbox.ee.extr(-100))
+                .f(None).s(hsl(0.9, 1, a=0.5)).sw(4)),
+            (DATText(gfn.glyph_name, Style("Times", 48, load_font=0),
+                render.rect.inset(50)))
+        ])
+
+preview_frame = Rect(1000, (-descender*2)+cap_height)
+
+@animation(preview_frame, timeline=Timeline(len(glyph_fns)), postfn=show_grid)
+def glyph_viewer(f):
+    glyph_fn = glyph_fns[f.i]
+    
+    ufo = DefconFont(ufo_path)
+    ufo.info.familyName = font_name
+    ufo.info.styleName = style_name
+    ufo.info.capHeight = cap_height
+    ufo.info.ascender = cap_height
+    ufo.info.descender = descender
+
+    print(f"> drawing :{glyph_fn.glyph_name}:")
+    glyph_pen = glyph_fn.func(glyph_fn.frame).f(0)
+
+    # shift over by the left-side-bearing
+    glyph_pen.translate(glyph_fn.lsb, 0)
+    glyph = glyph_pen.to_glyph(
+        name=glyph_fn.glyph_name,
+        width=glyph_fn.frame.w + glyph_fn.lsb + glyph_fn.rsb,
+        allow_blank = True)
+    glyph.unicode = glyph_to_uni(glyph_fn.glyph_name)
+    ufo.insertGlyph(glyph)
+    ufo.save()
+    return glyph_pen.add_data("gfn", glyph_fn)
+
+
+@renderable((1080, 300))
+def font_previewer(r):
+    """
+    This function loads the ufo that’s been created by the code
+    above and displays it "as a font" (i.e. it compiles the ufo
+    to a font and then uses the actual font to do standard font-
+    display logic)
+    """
+    ufo = Font(ufo_path)
+    return (StyledString("ABC CBA",
+        Style(ufo, 150))
+        .pens()
+        .align(r)
+        .f(0))
