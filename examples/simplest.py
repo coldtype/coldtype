a71d2632672789f33253f3b58703eb1ff0df8fa4
Rob Stenson <rob.stenson@gmail.com>
2021-05-18 12:11:02 -0700

    minimal release script

--- examples/simplest.py
+++ examples/simplest.py
@@ -1,9 +1,11 @@
 from coldtype import *
 
-@renderable(rect=(1200, 300))
+@renderable(rect=(1920, 1080))
 def render(r):
-    return (StyledString("COLDTYPE",
-        Style("assets/MutatorSans.ttf", 300))
-        .pen()
-        .align(r, tv=1)
-        .f(hsl(0.85)))
+    return DPS([
+        DP(r).f(0),
+        (StyledString("COLDTYPE",
+            Style("assets/MutatorSans.ttf", 300, wght=1))
+            .pen()
+            .align(r, tv=1)
+            .f(hsl(0.85, l=1)))])
---
+++ release.sh
@@ -0,0 +1,1 @@
+rm -rf dist/ && python setup.py sdist bdist_wheel && python -m twine upload dist/* --verbose
