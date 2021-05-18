d32b989f2bda744a68c8ff1a9aceb90aba25368a
Rob Stenson <rob.stenson@gmail.com>
2021-05-18 12:12:12 -0700

    whoops revert simplest.py

--- examples/simplest.py
+++ examples/simplest.py
@@ -1,11 +1,9 @@
 from coldtype import *
 
-@renderable(rect=(1920, 1080))
+@renderable(rect=(1200, 300))
 def render(r):
-    return DPS([
-        DP(r).f(0),
-        (StyledString("COLDTYPE",
-            Style("assets/MutatorSans.ttf", 300, wght=1))
-            .pen()
-            .align(r, tv=1)
-            .f(hsl(0.85, l=1)))])
+    return (StyledString("COLDTYPE",
+        Style("assets/MutatorSans.ttf", 300))
+        .pen()
+        .align(r, tv=1)
+        .f(hsl(0.85)))
