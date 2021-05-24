21e8e4efe2549224a47c812727bfd663947e21da
Rob Stenson <rob.stenson@gmail.com>
2021-05-24 09:14:51 -0700

    recursive composite simplification

--- examples/animations/recursive_composite.py
+++ examples/animations/recursive_composite.py
@@ -1,14 +1,21 @@
 from coldtype import *
 
-@animation((1200, 800), timeline=90, composites=1, solo=1)
+@renderable((1080, 1080))
+def border(r):
+    return DP(r).f(None).s(hsl(0.65)).sw(10)
+
+@animation((1080, 1080), timeline=90, composites=1, layer=True)
 def interpolation(f):
-    return (DATPens([
-        f.last_render(lambda p: p.translate(2, -2).sk_fill(1)),
-        (DP(Rect(100, 100))
-            .align(f.a.r, "mnx", "mxy")
-            .rotate(f.e()*-360)
-            .translate(50+f.a.r.w*0.66*f.e("ceio", 1), -50)
-            .f(0).s(1).sw(10)
-            #.f(hsl(0.8))
-            )
-        ]).phototype(f.a.r, fill=0, blur=3, cut=130, cutw=25))
+    return (DPS([
+        # f.last_render(lambda p: p
+        #     #.translate(1, -2)
+        #     #.scale(0.997)
+        #     .sk_fill(1)),
+        (DP(Rect(200, 200))
+            .align(f.a.r.inset(50), "mnx", "mxy")
+            #.rotate(f.e("eeio")*-360)
+            #.translate(f.a.r.w*0.66*f.e("ceio", 1), 0)
+            #.f(0).s(1).sw(10) # invert for phototype
+            )])
+        #.phototype(f.a.r, fill=hsl(0.9, 0.8), blur=3, cut=131, cutw=30)
+        )
