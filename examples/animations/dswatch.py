0d07d8cf053fa78165abbe98ecb4fdcc9ff790fe
Rob Stenson <rob.stenson@gmail.com>
2021-05-24 17:56:48 -0700

    dswatch simpler

--- examples/animations/dswatch.py
+++ examples/animations/dswatch.py
@@ -1,13 +1,22 @@
 from coldtype import *
+from functools import partial
 
 ds = Font("assets/ColdtypeObviously.designspace")
 
-@animation(timeline=120, watch=[ds], composites=1)
+def shake(idx, p):
+    """shake up the vector w/ a skia effect"""
+    return p.attr(skp=dict(
+        PathEffect=skia.DiscretePathEffect.Make(2, 2, idx)))
+
+@animation(timeline=(120, 23.976), watch=[ds])
 def dswatch(f):
     return (DPS([
-        (StSt("C", ds,
-            1000, ro=1, wdth=f.e("ceio", 1))
+        (StSt("COLD", ds, 510, ro=1, r=1,
+            wdth=f.e(1),
+            tu=250+-450*(f.e(1)))
             .align(f.a.r)
-            .f(0).s(1).sw(5))])
-        #.phototype(f.a.r, blur=2, cut=130, fill=0, cutw=20)
-        )
+            .f(0).s(1).sw(4)
+            .chain(partial(shake, f.i)))])
+        .phototype(f.a.r, blur=2,
+            cut=125, cutw=20,
+            fill=hsl(0.65)))
