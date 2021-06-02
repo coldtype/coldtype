a7db328eae70f0c5bc00b031a131ba7881d854e5
Rob Stenson <rob.stenson@gmail.com>
2021-06-02 08:16:41 -0700

    pappardelle at rest

--- examples/animations/colorfont.py
+++ examples/animations/colorfont.py
@@ -3,17 +3,18 @@ from coldtype import *
 ppvf = Font.Cacheable("~/Type/fonts/fonts/PappardelleParty-VF.ttf")
 
 def spin(fa, g):
-    y = 150
-    g[2].translate(0, fa.e(1, rng=(0, y)))
-    g[0].translate(0, fa.e(1, rng=(0, -y)))
-    g[1].rotate(fa.e(rng=(0, -360)))
+    y = 100
+    # address color font layers individually
+    #g[2].translate(0, fa.e(1, rng=(0, y)))
+    #g[0].translate(0, fa.e(1, rng=(0, -y)))
+    #g[1].rotate(fa.e(rng=(0, -360*2)))
 
 @animation((1080, 1920), timeline=120)
 def pappardelle(f):
     wave = (StSt("SPIN", ppvf, 500,
-        palette=0, SPIN=f.e("l"))
+        palette=0, _SPIN=f.e("l"))
         .align(f.a.r.take(0.4, "mny")))
-    
+
     r_wave = wave.ambit(th=1, tv=1)
 
     for idx, g in enumerate(wave):
