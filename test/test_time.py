9142bed860573cbf92b6d9d1c8860a18c252f33b
Rob Stenson <rob.stenson@gmail.com>
2021-06-07 11:04:12 -0700

    test looping in test

--- test/test_time.py
+++ test/test_time.py
@@ -6,13 +6,15 @@ at = AsciiTimeline(2, """
 [a          ]
         [b          ]
                 [c          ]
-                        [d          ]
+     ]                  [d          
 """)
 
 class TestTime(unittest.TestCase):
     def test_ascii_timeline(self):
+        print(at[-1])
         self.assertEqual(at[0].start, 0)
         self.assertEqual(at[1].end, 40)
+        self.assertEqual(at[-1].end, 84)
         self.assertEqual(len(at.clips), 4)
 
 if __name__ == "__main__":
