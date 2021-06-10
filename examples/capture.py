e9378e46259e050a48734f086ae772ee70e00346
Rob Stenson <rob.stenson@gmail.com>
2021-06-10 08:53:51 -0700

    note small changes

--- examples/capture.py
+++ examples/capture.py
@@ -1,3 +1,5 @@
+# a good invocation: coldtype examples/capture.py -wpass -wt -wo 0.85 -wp NW -ps 2
+
 from coldtype import *
 from coldtype.capture import read_frame
 from coldtype.midi.controllers import LaunchControlXL
@@ -11,13 +13,14 @@ def capture_with_midi(f:Frame, rs):
     img = (read_frame(rs.cv2caps[0]).align(f.a.r))
     
     return DPS([
-        img.a(0.5),
-        (StSt("MIDI", fnt, 200+nxl(10, 0)*500,
+        img.a(0.25),
+        (StSt("CTRL", fnt, 200+nxl(10, 0)*500,
             r=1, # reverse
             ro=1, # remove-overlap
-            grvt=nxl(21, 0),
+            rotate=nxl(60, 0)*360,
+            grvt=nxl(40, 0),
             yest=nxl(20, 0),
-            tu=(1-nxl(11, 0))*-300)
+            tu=(1-nxl(30, 0))*-650)
             .align(f.a.r.take(0.75, "mxy"))
             .f(hsl(0.7, 1, 0.7))
-            .understroke(sw=nxl(30, 0)*100))])
+            .understroke(sw=nxl(50, 0)*100))])
