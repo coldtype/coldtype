0f72bdfaf779ab6831a07610e5fbf63bbf8a2c78
Rob Stenson <rob.stenson@gmail.com>
2021-07-02 10:18:42 -0700

    versiof blender/varfont.py posted to twitter

--- examples/blender/varfont.py
+++ examples/blender/varfont.py
@@ -2,18 +2,18 @@ from coldtype import *
 from coldtype.blender import b3d, b3d_animation
 from coldtype.text.composer import Glyphwise
 
-fnt = Font.Cacheable("~/Type/fonts/fonts/Ehrie-V0_1-VF.ttf")
-fnt = Font.Cacheable("~/Type/fonts/fonts/NaNJauneMaxi-Var.ttf")
+fnt = Font.Cacheable("~/Type/fonts/fonts/SwearCilatiVariable.ttf")
 
 @b3d_animation(timeline=60)
 def varfont(f):
-    return (Glyphwise("Vari", lambda i,c:
-        Style(fnt, 350,
-            diss=f.adj(-i*5).e("seio", 1, rng=(0.98, 0)),
-            wght=f.adj(-i*5).e("seio", 1)))
+    return (Glyphwise("Variable", lambda i,c:
+        Style(fnt, 325,
+            opsz=f.adj(-i*5).e("seio", 1, rng=(0.98, 0)),
+            wght=f.adj(-i*15).e("seio", 1, rng=(0.98, 0))
+            ))
         .align(f.a.r)
-        .pmap(lambda i,p: p
-            .tag(f"L{i}")
-            .ch(b3d("Text", lambda bp: bp
+        .pmap(lambda i,p: p.tag(f"L{i}")
+            .chain(b3d("Text", lambda bp: bp
+                .metallic(0)
                 .extrude(f.adj(-i*5)
                     .e("ceio", 1, rng=(0.015, 3)))))))
