11b900d1a4fba633ad704e8851385980d550374b
Rob Stenson <rob.stenson@gmail.com>
2021-07-14 12:05:30 -0700

    minor changes to tapered shadow

--- examples/tapered_shadow.py
+++ examples/tapered_shadow.py
@@ -5,29 +5,34 @@ from coldtype.warping import warp_fn
 fnt = Font.Cacheable("assets/ColdtypeObviously-VF.ttf")
 
 rs = random_series(0, 1000)
-shake = 0
+shake = 1
 
-@animation(bg=1, timeline=Timeline(90, 23.976))
+@animation(bg=0, timeline=Timeline(90, 23.976))
 def taper(f):
     return ((StSt("COLD\nTYPE", fnt, 330,
-        wdth=f.e(1), leading=35, tu=60+f.e("eeo", 1, rng=(200, 30)), rotate=10)
+        rotate=10,
+        wdth=f.e(1),
+        leading=f.e(1, rng=(65, 45)),
+        tu=70+f.e(1, rng=(80, 0)))
         .align(f.a.r))
         .layer(
             lambda p: (p.pen().layer(
-                    lambda p: p.f(1).t(15+f.e(1, rng=(0, 10)), -15)
+                    lambda p: p.f(1).t(o:=15+f.e("seio", 1, rng=(0, 10)), -o)
                         .cond(shake, lambda p: p
                             .flatten(5)
                             .nlt(warp_fn(rs[f.i//4], mult=3))),
                     lambda p: p.fssw(0, 0, f.e(1, rng=(7, 11))))
                 .ch(phototype(f.a.r, blur=5, cut=183, cutw=8,
                     #fill=hsl(0.75, 0.94, 0.68)
-                    fill=rgb(0, 0, 1)
+                    #fill=rgb(0, 0, 1)
                     ))),
             lambda p: p.pen().f(1)
                 .cond(shake, lambda p: p
                     .flatten(5)
                     .nlt(warp_fn(rs[f.i//4+10], xs=100, mult=3)))
-                .ch(phototype(f.a.r, blur=3, cut=230, cutw=15,
+                .ch(phototype(f.a.r, blur=3,
+                    cut=230+f.e(1, rng=(-30, 5)),
+                    cutw=15,
                     #fill=hsl(0.2, 0.86, 0.63)
-                    fill=rgb(1, 1, 1)
+                    #fill=rgb(1, 0, 0)
                     ))))
