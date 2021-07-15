f19947c18e55208632f5edd2e08e85c388b02863
Rob Stenson <rob.stenson@gmail.com>
2021-07-14 20:09:47 -0700

    more futzing with taper

--- examples/tapered_shadow.py
+++ examples/tapered_shadow.py
@@ -10,22 +10,22 @@ rs = random_series(0, 1000)
 def taper(f):
     return (StSt("COLD\nTYPE", fnt, 330,
         rotate=f.e(1, rng=(5, 10)),
-        wdth=f.e("ceo", 1),
+        wdth=f.e(1),
         leading=f.e(1, rng=(65, 45)),
         tu=70+f.e(1, rng=(80, 0)))
         .align(f.a.r)
         .pen()
         .cond(SHAKE, warp(-1, rs[f.i//4+10], mult=5))
         .layer(
-            lambda p: (p.pen()
-                .layer(
-                    lambda p: p
-                        .f(1)
-                        .t(o:=15+f.e(1, rng=(0, 10)), -o)
-                        .cond(SHAKE, warp(-1, rs[f.i//4], mult=5)),
-                    lambda p: p.fssw(0, 0, f.e(1, rng=(7, 11))))
-                    .ch(phototype(f.a.r, blur=5, cut=183, cutw=8,
-                        fill=hsl(0.75, 0.94, 0.68)))),
+            lambda p: (p.pen().layer(
+                lambda p: p
+                    .f(1)
+                    .t(o:=15+f.e(1, rng=(0, 10)), -o)
+                    .cond(SHAKE, warp(-1, rs[f.i//4], mult=5)),
+                lambda p: p
+                    .fssw(0, 0, f.e(1, rng=(7, 11))))
+                .ch(phototype(f.a.r, blur=5, cut=183, cutw=8,
+                    fill=hsl(0.75, 0.94, 0.68)))),
             lambda p: p.pen().f(1)
                 #.cond(SHAKE, warp(-1, rs[f.i//4+10], mult=5))
                 .ch(phototype(f.a.r, blur=3,
