301d482774742a3ad96baebdad7e7b6371a547d2
Rob Stenson <rob.stenson@gmail.com>
2021-07-19 17:12:19 -0700

    slight updates to nonvarinterp.py

--- examples/animations/nonvarinterp.py
+++ examples/animations/nonvarinterp.py
@@ -21,7 +21,6 @@ b = build(mdpb)
 @b3d_animation(r, timeline=Timeline(90))
 def nonvarinterp(f):
     i = "{:.7f}".format(f.e("eeio", 1))
-    ro = -360*f.e("l", 3, cyclic=0)
 
     return DPS([
         (StSt(i, mdiob, 72)
@@ -33,21 +32,16 @@ def nonvarinterp(f):
                 .extrude(f.e(1, rng=(0.01, 0.5)))
                 .emission(hsl(0.65, 1, 0.3), 1)))),
         (a.interpolate(f.e("eeio", 1), b)
+            .mod_contour(18, lambda p: p
+                .rotate(f.e("l", 3, cyclic=0, rng=(0, -360))))
             .f(hsl(0.4, 1, 0.3))
-            .mod_contour(18, lambda p:p.rotate(ro))
-            #.mod_contour(4, lambda p:p.rotate(ro))
             .removeOverlap()
             .tag("Interpolation")
             .ch(b3d("Text", lambda bp: bp
                 .extrude(f.e("eeio", 1, rng=(0.01, 3))))))])
 
 def build(_):
-    #print(__FILE__)
-    # write render cmd to blender.txt
-    # watch.py then takes care of calling the actual render
-    # monitor for files as they come in?
-    # monitor for crash file?
-    #pass
+    # sketchy for now but works
     cb = Path("~/.coldtype/blender.txt").expanduser()
     if cb.exists():
         cb.unlink()
