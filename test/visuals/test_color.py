cacf2b9b30e0f91453efb42f9688f7f2134a75b6
Rob Stenson <rob.stenson@gmail.com>
2021-07-28 10:05:19 -0700

    longer color test

--- test/visuals/test_color.py
+++ test/visuals/test_color.py
@@ -10,9 +10,9 @@ def hsl_interp(f):
         .f(hsl(0.5).hsl_interp(p, hsl(0.1))))
 
 
-@animation()
+@animation(timeline=120)
 def rgb_interp(f):
-    p = f.a.progress(f.i, easefn="qeio").e
+    p = f.a.progress(f.i, loops=1, easefn="eeio").e
     return (DATPen()
         .rect(f.a.r.inset(100))
         .f(hsl(0.5).rgb_interp(p, hsl(0.8))))
