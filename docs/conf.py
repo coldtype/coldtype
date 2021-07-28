3ca22a1dc8a9ca85f430069cd74e28d94d72938c
Rob Stenson <rob.stenson@gmail.com>
2021-07-28 11:32:16 -0700

    0.5.11

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Rob Stenson, Goodhertz'
 author = 'Rob Stenson, Goodhertz'
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.10"
+release = "0.5.11"
 
 
 # -- General configuration ---------------------------------------------------
