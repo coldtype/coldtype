e55186b41e6abe892c3817c1a7a5e4f0e2cfabad
Rob Stenson <rob.stenson@gmail.com>
2021-07-28 10:59:18 -0700

    officehours a little more fun with eeio

--- examples/animations/officehours.py
+++ examples/animations/officehours.py
@@ -13,8 +13,8 @@ at = AsciiTimeline(1, """
 @animation(timeline=at)
 def officehours(f):
     stx = Style.StretchX(0,
-        L=(730*at["L"].io2(f.i, 10, e:=["eeo", "eeio"]), 310),
-        F=(1220*at["F"].io2(f.i, 10, ["eei", "eeo"]), 260),
+        L=(730*at["L"].io2(f.i, 10, e:="ceio"), 310),
+        F=(1220*at["F"].io2(f.i, 10, e), 260),
         H=(1900*at["H"].io2(f.i, 10, e), 250))
     
     txt = (StSt("Coldtype\nOffice\nHours".upper(),
@@ -26,7 +26,8 @@ def officehours(f):
             .f(hsl(0.35, 0.8, 0.3)))
 
     # after bg, so it doesn't effect bounds 
-    txt.index([0, 2], lambda p: p.rotate(f.e(0, cyclic=0, rng=(0, 360))))
+    txt.index([0, 2], lambda p: p
+        .rotate(f.e(0, cyclic=0, rng=(0, 360))))
 
     return DPS([
         bg.copy().translate(5, -5).f(0),
