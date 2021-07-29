2c17fb26126b6685526c21e9c46638d58ec4ec15
Rob Stenson <rob.stenson@gmail.com>
2021-07-28 17:40:05 -0700

    bump to 0.5.12

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Rob Stenson, Goodhertz'
 author = 'Rob Stenson, Goodhertz'
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.11"
+release = "0.5.12"
 
 
 # -- General configuration ---------------------------------------------------
