18764f60410e32eefd3735f2049e4b40a1f8b579
Rob Stenson <rob.stenson@gmail.com>
2021-09-07 19:28:25 -0700

    glyphwise example with dual-style return

---
+++ examples/animations/glyphwise.py
@@ -0,0 +1,25 @@
+from coldtype import *
+from coldtype.fx.skia import phototype
+
+fnt = Font.MutatorSans()
+
+@animation(timeline=90, bg=0)
+def glyphwise(f):
+    def styler(g):
+        return [
+            Style(fnt, 250, wdth=0.15),
+            Style(fnt, 250,
+                wdth=f.adj(-g.i*2).e("eeio", 2),
+                wght=f.adj(-g.i*3).e("seio", 3),
+                ro=1,
+                tv=1)]
+
+    return (Glyphwise("COLD\nTYPE", styler)
+        .xalign(f.a.r, th=0)
+        .track(30, v=1)
+        .align(f.a.r, th=0)
+        .collapse()
+        .reverse_pens()
+        .f(1)
+        .understroke(sw=5)
+        .ch(phototype(f.a.r, 3, 190, cutw=20)))
