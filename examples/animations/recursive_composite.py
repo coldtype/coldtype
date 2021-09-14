60c6124970462e77715ddb8eb0eafee833718800
Rob Stenson <rob.stenson@gmail.com>
2021-09-14 14:29:23 -0700

    no border on recursive composite

--- examples/animations/recursive_composite.py
+++ examples/animations/recursive_composite.py
@@ -1,11 +1,7 @@
 from coldtype import *
 from coldtype.fx.skia import fill, phototype
 
-@renderable((1080, 1080))
-def border(r):
-    return DP(r).f(None).s(hsl(0.65)).sw(10)
-
-@animation((1080, 1080), timeline=90, composites=1, layer=True)
+@animation((1080, 1080), timeline=90, composites=1)
 def recursive_composite(f):
     return (DPS([
         f.last_render(lambda p: p
@@ -13,11 +9,10 @@ def recursive_composite(f):
             .scale(0.997)
             .ch(fill(1))),
         (DP(Rect(200, 200))
-            .align(f.a.r.inset(50), "mnx", "mxy")
+            .align(f.a.r.inset(100, 100), "mnx", "mxy")
             .rotate(f.e("eeio")*-360)
-            .translate(f.a.r.w*0.66*f.e("ceio", 1), 0)
+            .translate(f.a.r.w*0.6*f.e("ceio", 1), 0)
             .f(0).s(1).sw(10) # invert for phototype
             )])
         .ch(phototype(f.a.r,
-            fill=hsl(0.90, 0.8), blur=3, cut=131, cutw=30))
-        )
+            fill=hsl(0.90, 0.8), blur=3, cut=133, cutw=30)))
