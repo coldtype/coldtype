ff2126cf6b7bf5e013bfad68731325ba335de942
Rob Stenson <rob.stenson@gmail.com>
2021-09-28 09:42:24 -0700

    add the .coldtype.py to the git repo lol

---
+++ coldtype/renderer/.coldtype.py
@@ -0,0 +1,22 @@
+# Some common profiles
+
+PROFILES = dict(
+    b3d = dict(
+        WINDOW_PIN = "SE",
+        WINDOW_OPACITY = 0.75,
+        WINDOW_CHROMELESS = True,
+        WINDOW_PASSTHROUGH = True,
+        WINDOW_FLOAT = True,
+        PREVIEW_SCALE = 0.5,
+        BLENDER_WATCH = 1,
+    ),
+    tut = dict(
+        WINDOW_PIN = "NW",
+        WINDOW_OPACITY = 0.85,
+        WINDOW_CHROMELESS = True,
+        WINDOW_TRANSPARENT = True,
+        WINDOW_PASSTHROUGH = True,
+        WINDOW_FLOAT = True,
+        PREVIEW_SCALE = 1
+    ),
+)
