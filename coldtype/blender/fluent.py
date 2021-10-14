181bca321449ddc1095d801d90812511bcbb1103
Rob Stenson <rob.stenson@gmail.com>
2021-10-13 18:47:17 -0700

    move mods to fluent obj

--- coldtype/blender/fluent.py
+++ coldtype/blender/fluent.py
@@ -315,3 +315,73 @@ class BpyObj(_Chainable):
                 bpy.ops.rigidbody.bake_to_keyframes()
         
         return self
+    
+    def solidify(self, thickness=1):
+        bpy.context.view_layer.objects.active = None
+        bpy.context.view_layer.objects.active = self.obj
+        self.obj.select_set(True)
+        if "Solidify" not in self.obj.modifiers:
+            bpy.ops.object.modifier_add(type="SOLIDIFY")
+        self.obj.modifiers["Solidify"].thickness = thickness
+        self.obj.select_set(False)
+        bpy.context.view_layer.objects.active = None
+        return self
+    
+    def remesh(self, octree_depth=7, smooth=False):
+        with self.obj_selected():
+            bpy.ops.object.modifier_add(type="REMESH")
+            
+            m = self.obj.modifiers["Remesh"]
+            m.mode = "SHARP"
+            m.octree_depth = octree_depth
+            m.use_remove_disconnected = False
+            m.use_smooth_shade = smooth
+        return self
+    
+    def displace(self, strength=1, midlevel=0.5, texture=None, coords_object=None, direction="NORMAL"):
+        with self.obj_selected():
+            bpy.ops.object.modifier_add(type="DISPLACE")
+            
+            m = self.obj.modifiers["Displace"]
+            m.strength = strength
+            m.mid_level = midlevel
+            m.direction = direction
+
+            if texture and isinstance(texture, str):
+                try:
+                    t = bpy.data.textures[texture]
+                    m.texture = t
+                except KeyError:
+                    bpy.ops.texture.new()
+                    t = bpy.data.textures[len(bpy.data.textures)-1]
+                    t.name = texture
+                    t.type = "CLOUDS"
+                    m.texture = t
+            if coords_object:
+                try:
+                    m.texture_coords = "OBJECT"
+                    m.texture_coords_object = bpy.data.objects[coords_object]
+                except KeyError:
+                    print("coords_object not found", coords_object)
+        return self
+    
+    def boolean(self, object):
+        with self.obj_selected():
+            bpy.ops.object.modifier_add(type="BOOLEAN")
+            m = self.obj.modifiers["Boolean"]
+            m.operation = "INTERSECT"
+            try:
+                m.object = bpy.data.objects[object]
+            except KeyError:
+                print("object for boolean not found", object)
+        return self
+    
+    def shade_smooth(self):
+        with self.obj_selected():
+            bpy.ops.object.shade_smooth()
+        return self
+    
+    def subsurface(self):
+        with self.obj_selected():
+            bpy.ops.object.modifier_add(type="SUBSURF")
+        return self
