974ab5a593e5f92820493afcce7d1b87d2d1b339
Rob Stenson <rob.stenson@gmail.com>
2021-10-27 15:09:14 -0700

    rewrite twostep with built-in font

--- examples/animations/ascii_simple.py
+++ examples/animations/ascii_simple.py
@@ -1,25 +1,21 @@
 from coldtype import *
 from coldtype.time.nle.ascii import AsciiTimeline
 
-fnt = Font.Find("CheeeV")
-
-at = AsciiTimeline(1, """
-                                                              <
-          [a                                 ]
-                      [b                     ]
-                                [c                   ]
+at = AsciiTimeline(4, """
+                                <
+[B    ]
+            [A          ]
+                [C         ]
+                    [wght     ]
 """)
 
-@animation((1080, 540), timeline=at, bg=1)
-def test_ascii(f):
-    io = at.io2(f.i, 20, "eeio")
-    word = (Glyphwise("ASCII", lambda g:
-        Style(fnt, 100,
-            grvt=(at[g.i > 1].io2(f.i, 10, "eeio")),
-            yest=at["c"].io2(f.i, 8, "eeio")))
-        .align(f.a.r)
-        .scale(io*2))
-    
-    return PS([
-        P(word.ambit(tv=1).inset(-50)).fssw(-1, 0, 2),
-        word.f(0)])
+@animation((1080, 540), timeline=at, bg=0.97, render_bg=1)
+def ascii(f):
+    def wdth(g):
+        return at[g.c].io2(f.i, 8, "eeio")
+
+    return (Glyphwise("ABC", lambda g:
+        Style(Font.MutatorSans(), 250,
+            wdth=wdth(g),
+            wght=at["wght"].io2(f.i, 10, "eeio")))
+        .align(f.a.r))
---
+++ examples/animations/ascii_twostep.py
@@ -0,0 +1,25 @@
+from coldtype import *
+from coldtype.time.nle.ascii import AsciiTimeline
+
+fnt = Font.ColdtypeObviously()
+
+at = AsciiTimeline(1, """
+                                                              <
+          [a                                 ]
+                      [b                     ]
+                                [c                   ]
+""")
+
+@animation((1080, 540), timeline=at, bg=1)
+def test_ascii(f):
+    io = at.io2(f.i, 20, "eeio")
+    word = (Glyphwise("TYPE", lambda g:
+        Style(fnt, 100,
+            wdth=(at[g.i > 1].io2(f.i, 10, "eeio")),
+            tu=at["c"].io2(f.i, 8, "eeio")*500))
+        .align(f.a.r)
+        .scale(io*2))
+    
+    return PS([
+        P(word.ambit(tv=1).inset(-50)).fssw(-1, 0, 2),
+        word.f(0)])
