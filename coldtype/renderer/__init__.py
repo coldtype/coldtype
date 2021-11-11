4935f6f36d3d6cd77755d348ecf10f20b9555de9
Rob Stenson <rob.stenson@gmail.com>
2021-11-11 09:06:06 -0800

    move dup-prevention to all files in mod watch callback

--- coldtype/renderer/__init__.py
+++ coldtype/renderer/__init__.py
@@ -1218,7 +1218,18 @@ class Renderer():
         if path.parent in self.watchee_paths():
             actual_path = path
             path = path.parent
+        
         if path in self.watchee_paths():
+            last = self.watchee_mods.get(path)
+            now = ptime.time()
+            self.watchee_mods[path] = now
+            if last is not None:
+                diff = now - last
+                if diff < 0.05:
+                    return
+                else:
+                    pass
+
             if path.suffix == ".json":
                 if path.stem == "command":
                     data = json.loads(path.read_text())
@@ -1232,15 +1243,6 @@ class Renderer():
                         self.hotkey_waiting = (action, None, None)
                     return
 
-                last = self.watchee_mods.get(path)
-                now = ptime.time()
-                self.watchee_mods[path] = now
-                if last is not None:
-                    diff = now - last
-                    if diff < 0.25:
-                        return
-                    else:
-                        pass
                 try:
                     json.loads(path.read_text())
                 except json.JSONDecodeError:
--- examples/simple.py
+++ examples/simple.py
@@ -5,12 +5,12 @@ def render(r):
     c1 = hsl(0.65, 0.7)
     c2 = hsl(0.53, 0.6)
 
-    return DATPens([
-        (DATPen(r.inset(10))
+    return PS([
+        (P(r.inset(10))
             .outline(10)
             .f(Gradient.H(r, c2.lighter(0.3), c1.lighter(0.3)))),
         (StSt("COLDTYPE", "assets/ColdtypeObviously-VF.ttf", 250,
-                wdth=1, tu=-170, r=1, rotate=15)
+                wdth=1, tu=-250, r=1, rotate=15)
             .align(r)
             .f(Gradient.H(r, c1, c2))
             .understroke(s=1, sw=5))
