4ecb4286fe8408ddbd7ea45fdc0bc96101347564
Rob Stenson <rob.stenson@gmail.com>
2021-12-06 12:55:09 -0800

    notes on no_command_line.py

--- examples/misc/no_command_line.py
+++ examples/misc/no_command_line.py
@@ -1,9 +1,16 @@
 from coldtype.renderer import Renderer
 
 """
+`python examples/misc/no_command_line.py`
+
 An example of how to call the Coldtype renderer directly,
 without the need to use the command line at all
 (mostly useful if you’re batch-rendering fully written animations)
+
+That said, if you take out the "-a" arg, this should run exactly
+like the normal coldtype viewer — potentially useful for a situation
+in which it’s difficult to access the `coldtype` bin command, since
+you could run this with just the standard `python` bin command
 """
 
 _, parser = Renderer.Argparser()
