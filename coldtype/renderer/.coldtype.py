49c6ddb15136e57a80d063509aca86c39bb281df
Rob Stenson <rob.stenson@gmail.com>
2021-12-06 09:42:28 -0800

    b3dlo profile

--- coldtype/renderer/.coldtype.py
+++ coldtype/renderer/.coldtype.py
@@ -10,6 +10,17 @@ PROFILES = dict(
         PREVIEW_SCALE = 0.5,
         BLENDER_WATCH = 1,
     ),
+    b3dlo = dict(
+        WINDOW_PIN = "SE",
+        WINDOW_OPACITY = 0.75,
+        WINDOW_CHROMELESS = True,
+        WINDOW_PASSTHROUGH = True,
+        WINDOW_FLOAT = True,
+        PREVIEW_SCALE = 0.5,
+        BLENDER_WATCH = 1,
+        LOAD_ONLY = 1,
+        NO_VIEWER = 1,
+    ),
     tut = dict(
         WINDOW_PIN = "NW",
         WINDOW_OPACITY = 0.85,
