e5ae25632696e611cce710bff552284e26c3b4e2
Rob Stenson <rob.stenson@gmail.com>
2021-12-09 15:40:44 -0800

    fix up countdown, force add scratchedfilm.py

--- examples/animations/countdown.py
+++ examples/animations/countdown.py
@@ -1,11 +1,13 @@
-from coldtype.test import *
-from coldtype.fx.skia import phototype, spackle, color_phototype
+from coldtype import *
+from coldtype.fx.skia import phototype, spackle
 from coldtype.fx.motion import filmjitter
 
 at = AsciiTimeline(2, 18, """
                                 <
 [C ][O ][L ][D ][T ][Y ][P ][E ]
 """)
+
+rs = random_series(seed=8)
 
 @animation((1080, 1080)
     , tl=at
@@ -16,49 +18,20 @@ def countdown(f:Frame):
     c = at.current()
 
     return (P(f.a.r)
-        .ch(spackle(xs=0.35, ys=0.35, cut=230, fill=bw(0), base=f.i))
+        .ch(spackle(xs=0.35, ys=0.35, cut=190, fill=bw(0), base=f.i))
         .ups()
         .insert(0, P(f.a.r).f(1, 0.1))
         .insert(0, f.last_render(
-            filmjitter(f.a.progress(f.i).e, speed=(50, 50), scale=(3, 5))))
-        .append(StSt(c.name
-            , Font.Find("ObviouslyV")
-            , c.e("l", 0, rng=(1, 1500))
-            , wdth=f.e("seio", 1, rng=(1, 0.25))
-            #, wght=f.e("seio", 0, rng=(1, 0))
-            , wght=0.25
-            , ro=1
-            , ss01=1)
-            #.pen()
+            filmjitter(f.e("l"), speed=(50, 50), scale=(3, 5))))
+        .append(StSt(c.name, Font.MutatorSans()
+            , c.e("l", 0, rng=(1, 1000))
+            , wdth=rs[c.idx]
+            , wght=rs[c.idx+1]*0.25
+            , ro=1)
             .fssw(1, 1, 25)
             .align(f.a.r, th=1, tv=1)
-            #.cond(c.name == "C",
-            #    lambda p: p.insert(0,
-            #        P(f.a.r).f(0)))
-            .blendmode(BlendMode.Cycle(22, show=0)))
-        .cond(f.i == at.timeables[0].end-1,
-            lambda p: p.insert(2, P(f.a.r).f(0)))
+            .blendmode(BlendMode.Xor))
         .ch(phototype(f.a.r
-            , cut=150
-            , blur=2.5
-            , cutw=20)))
-
-    c = at.current()
-    return (StSt(c.name
-        , Font.ColdObvi()
-        , c.e("eeo", 0, rng=(0, 1200))
-        , wdth=1
-        , ro=1)
-        .align(f.a.r)
-        .fssw(0, 1, 20)
-        .blendmode(BlendMode.Xor)
-        #.f(1)
-        .append(P(f.a.r).ch(spackle(cut=155, cutw=1, fill=bw(0), base=f.i)))
-        .ch(phototype(f.a.r, 2, cutw=1))
-        .ups()
-        .insert(0, f.lastRender(lambda p: p
-            .ch(filmjitter(f.e("l", 0), speed=(50, 50)))))
-        .insert(1, P(f.a.r).f(0, 0.25))
-        .ch(phototype(f.a.r, 2, cut=151, cutw=20)))
+            , cut=150, blur=2.5, cutw=20)))
     
-release = countdown.export("h264", loops=2)
+release = countdown.export("h264", loops=4)
---
+++ examples/animations/scratchedfilm.py
@@ -0,0 +1,18 @@
+from coldtype import *
+from coldtype.fx.skia import spackle, phototype
+
+@animation(bg=0)
+def scratches(f):
+    return PS([
+        (P(f.a.r)
+            .ch(spackle(cut=235, cutw=1))
+            .ch(phototype(f.a.r,
+                blur=3, cut=120, fill=hsl(0.35, 1)))),
+        (P(f.a.r)
+            .ch(spackle(cut=235, cutw=30))
+            .ch(phototype(f.a.r,
+                blur=3, cut=120, fill=hsl(0.85, 1)))),
+        (P(f.a.r)
+            .ch(spackle(cut=205, cutw=1))
+            .ch(phototype(f.a.r,
+                blur=10, cut=150, cutw=20, fill=hsl(0.17, 1))))])
