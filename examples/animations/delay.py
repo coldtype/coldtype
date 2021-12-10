905c3fb4cc08ba3bae72ee41ec1d8400f15c5aae
Rob Stenson <rob.stenson@gmail.com>
2021-12-10 10:08:30 -0800

    f.e for delay.py

--- examples/animations/delay.py
+++ examples/animations/delay.py
@@ -4,16 +4,18 @@ from coldtype.fx.skia import color_phototype
 
 @animation((1500, 800), timeline=Timeline(60, fps=23.976))
 def recur(f, depth=0):
-    t = f.a.progress(f.i, loops=1, easefn="qeio") 
     cold = (StSt("COLDTYPE", Font.ColdObvi()
-            , fontSize=800-t.e*700
-            , wdth=t.e
-            , tu=-90+t.e*50
+            , fontSize=f.e("qeio", r=(800, 100))
+            , wdth=f.e("qeio")
+            , tu=f.e("qeio", r=(-90, -40))
             , ro=1
             , r=1)
         .align(f.a.r)
         .fssw(1, 0, 23-depth)
-        .ch(color_phototype(f.a.r, blur=2+depth*5, cut=120+depth*5, rgba=[1, 0, 1, 1]))
+        .ch(color_phototype(f.a.r
+            , blur=2+depth*5
+            , cut=120+depth*5
+            , rgba=[1, 0, 1, 1]))
         .ups()
         .blendmode(BlendMode.Luminosity))
     
