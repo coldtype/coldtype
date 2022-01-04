601c041495bc35aae31379fb7c445f63a9e6d4d9
Rob Stenson <rob.stenson@gmail.com>
2022-01-04 09:54:54 -0800

    move minimal RunonPen def into file

--- coldtype/pens/runonpen.py
+++ coldtype/pens/runonpen.py
@@ -1,7 +1,52 @@
-from coldtype.runon.runon import Runon
+
 from fontTools.pens.recordingPen import RecordingPen
 
+from coldtype.geometry import Rect
+from coldtype.runon.runon import Runon
 
-class RunonPen(Runon):
-    def post_init(self):
-        pass
+from coldtype.pens.mixins.StylingMixin import StylingMixin
+from coldtype.pens.mixins.LayoutMixin import LayoutMixin
+
+class RunonPen(Runon,
+    StylingMixin,
+    LayoutMixin
+    ):
+    def FromPens(pens):
+        if hasattr(pens, "_pens"):
+            out = RunonPen()
+            for p in pens:
+                rp = RecordingPen()
+                p.replay(rp)
+                rrp = RunonPen(rp)
+                if p._frame:
+                    rrp.data(frame=p._frame)
+                out.append(rrp)
+        else:
+            out = RunonPen(pens)
+            if hasattr(pens, "_frame"):
+                out.data(frame=pens._frame)
+        return out
+    
+    def __init__(self,
+        value=None,
+        els=None,
+        data=None,
+        attrs=None
+        ):
+        r = None
+        if isinstance(value, Rect):
+            r = value
+            value = None
+        
+        super().__init__(value=value, els=els, data=data, attrs=attrs)
+
+        if r:
+            self.rect(r)
+    
+    def printable_val(self):
+        if self._val:
+            return f"RecordingPen({len(self._val.value)})"
+
+    def style(self, style="_default"):
+        st = {**super().style(style)}
+        return self.groupedStyle(st)
