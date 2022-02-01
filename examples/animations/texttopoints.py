15760e5f18f86b353a8f5fa4aadd2eb067c04dd1
Rob Stenson <rob.stenson@gmail.com>
2022-02-01 09:38:47 -0800

    texttopoints.py example

---
+++ examples/animations/texttopoints.py
@@ -0,0 +1,15 @@
+from coldtype import *
+from coldtype.fx.skia import phototype
+
+# inspired by https://p5js.org/reference/#/p5.Font/textToPoints
+
+@animation(timeline=126, bg=hsl(0.6, 1, 0.33))
+def texttopoints(f):
+    return (StSt("COLD", Font.ColdObvi(), 1000, wdth=0)
+        .align(f.a.r)
+        .pen()
+        .removeOverlap()
+        .flatten(10)
+        .nonlinear_transform(lambda x, y: (x+math.sin(f.i+(y*0.15))*10, y))
+        .f(1)
+        .ch(phototype(f.a.r, fill=hsl(0.6, 1, 0.7), cutw=10)))
