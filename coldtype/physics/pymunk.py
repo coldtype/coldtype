52df2a6d161de3d11857ed6741b262d8938afd1d
Rob Stenson <rob.stenson@gmail.com>
2022-02-10 13:27:21 -0800

    another speculative pymunk helper

--- coldtype/physics/pymunk.py
+++ coldtype/physics/pymunk.py
@@ -2,6 +2,20 @@ import pymunk
 from coldtype.runon.path import P
 
 
+def polygon(samples=5):
+    def _polygon(p:P):
+        pts = list(map(lambda x: x.pt.xy(), p.pen().samples(samples)))
+        pmass = 3.0
+        pmoment = pymunk.moment_for_poly(pmass, pts)
+        pbody = pymunk.Body(pmass, pmoment)
+        pshape = pymunk.Poly(pbody, pts)
+        pshape.friction = 0.01
+        pshape.elasticity = 0.9
+        pshape.collision_type = 0
+        return pbody, pshape
+    return _polygon
+
+
 def segments(body, flatten=30):
     def _segments(p:P):
         p = p.copy().pen().removeOverlap()
