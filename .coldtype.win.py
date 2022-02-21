3909f7b53bed1f3603956362586033499c5ad90a
Rob Stenson <rob@goodhertz.com>
2022-02-21 13:36:40 -0800

    windows-specific config file

---
+++ .coldtype.win.py
@@ -0,0 +1,12 @@
+
+WINDOW_PIN = "SE"
+WINDOW_CONTENT_SCALE = 1
+WINDOW_FLOAT = 1
+#WINDOW_TRANSPARENT = 1
+
+HOTKEYS = {
+    "<ctrl>+<f1>": "restart_app"
+}
+
+from pathlib import Path
+BLENDER_APP_PATH = Path("C:/Program Files/Blender Foundation/Blender 3.0/blender.exe").resolve()
