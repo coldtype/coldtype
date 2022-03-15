4289066a1887cb2a81de74efcccab1b2e1946411
Rob Stenson <rob.stenson@gmail.com>
2022-03-15 15:58:53 -0700

    make sure it's a string

--- coldtype/renderable/tools.py
+++ coldtype/renderable/tools.py
@@ -3,7 +3,7 @@ from pathlib import Path
 FFMPEG_COMMAND = "ffmpeg"
 
 def set_ffmpeg_command(cmd):
-    if "/" in cmd:
+    if isinstance(cmd, str) and "/" in cmd:
         cmd = str(Path(cmd).expanduser().absolute())
 
     global FFMPEG_COMMAND
