abd00b6c6632df482c69eddc7f11e61c559bcc4a
Rob Stenson <rob.stenson@gmail.com>
2022-03-24 15:07:23 -0700

    modernize test_midi_ctrl

--- test/visuals/test_midi_ctrl.py
+++ test/visuals/test_midi_ctrl.py
@@ -1,36 +1,20 @@
 from coldtype import *
-from coldtype.midi.controllers import LaunchControlXL, LaunchkeyMini
-
-obv = Font.Cacheable("~/Type/fonts/fonts/ObviouslyVariable.ttf")
-
-from pprint import pprint
+from coldtype.midi.controllers import LaunchControlXL
 
 @animation((1080, 1080), bg=1, rstate=1)
 def render(f, rstate):
-    r = f.a.r
     nxl = LaunchControlXL(rstate.midi)
-    nmn = LaunchkeyMini(rstate.midi)
-
-    style = Style(obv,
-        20+nxl(12, 0.25)*2000,
-        wdth=nxl(11, 0.25),
-        wght=nxl(21),
-        slnt=nxl(31),
-        tu=-250+nxl(22)*500,
-        r=1,
-        ro=1)
-
-    dps = (StyledString("Midi", style)
-        .pens()
-        .align(r)
-        .f(1))
-
-    g1 = Gradient.Vertical(r, hsl(nxl(20, 0.25), a=0.25), hsl(nxl(10, 0.45), a=0.25))
     
-    return [
-        (DATPen().rect(r).f(g1)),
-        # (dps.copy().pen()
-        #     #.castshadow(nxl(41, 0.3)*100-50, nxl(51, 1)*1000).f(0)
-        #     .f(Gradient.Vertical(r, hsl(nxl(30, 0.66)), hsl(nxl(40, 0.5))))),
-        dps.understroke(s=g1, sw=nxl(71)*20)
-    ]
+    return P(
+        (P(f.a.r).f(Gradient.V(f.a.r,
+            hsl(nxl(20, 0.25), a=0.5),
+            hsl(nxl(10, 0.45), a=0.5)))),
+        (StSt("MIDI", Font.MutatorSans()
+            , fontSize=20+nxl(12, 0.25)*500
+            , wdth=nxl(11, 0.25)
+            , wght=nxl(21)
+            , tu=-250+nxl(22)*500
+            , r=1
+            , ro=1)
+            .align(f.a.r, tv=1)
+            .f(1)))
