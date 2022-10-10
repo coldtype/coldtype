477149875b71ec6facbc6a4b91320b8605888f2f
Rob Stenson <rob.stenson@gmail.com>
2022-10-10 11:20:17 -0700

    convert shapes to ipynb

--- docs/download_colabs.py
+++ docs/download_colabs.py
@@ -4,12 +4,14 @@ from urllib.parse import urlparse
 from pathlib import Path
 
 notebooks = {
-    "tutorials/geometry": 
-        "1ldEBGu6z5kJBamnpCA1D71fecpZyFbPs",
-    "tutorials/text":
-        "1E-q_UdRFkxQRI7Lx6OxDfTw_WBD4cyuI",
-    "tutorials/animation":
-        "1sxNSdggg7mZmkQgSXG2WB2LwwtHA1UiK"
+    # "tutorials/geometry": 
+    #     "1ldEBGu6z5kJBamnpCA1D71fecpZyFbPs",
+    # "tutorials/text":
+    #     "1E-q_UdRFkxQRI7Lx6OxDfTw_WBD4cyuI",
+    # "tutorials/animation":
+    #     "1sxNSdggg7mZmkQgSXG2WB2LwwtHA1UiK",
+    "tutorials/shapes":
+        "1mJ1QpXBSvf7gtSxQmpP8PCG_huit5_Ri"
 }
 
 for k, url in notebooks.items():
