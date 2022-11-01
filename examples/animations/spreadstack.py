c096ece7831d162c77eb03cfa1a60d9161c29bd2
Rob Stenson <rob.stenson@gmail.com>
2022-11-01 09:54:22 -0700

    spreadstack.py example

---
+++ examples/animations/spreadstack.py
@@ -0,0 +1,19 @@
+from coldtype import *
+
+@animation((1080, 540), 60, bg=1)
+def scratch(f):
+    ex = f.e("cei", r=(0, 60))
+    rs = random_series(-ex, ex, f.i)
+
+    return (P()
+        .rect(Rect(100, 100))
+        .difference(P(Rect(100, 100))
+            .t(-50, -50)
+            .rotate(20))
+        .layer(5)
+        .spread(-10)
+        .layer(3)
+        .stack(-10)
+        .fssw(hsl(0.8, 1, a=0.3), 0, 1)
+        .align(f.a.r)
+        .mapv(lambda i, p: p.rotate(rs[i])))
