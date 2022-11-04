44fb6237c4ce1e1b968bf62d909ab9c9562879cd
Rob Stenson <rob.stenson@gmail.com>
2022-11-04 09:23:58 -0700

    mirroring example

---
+++ examples/mirror.py
@@ -0,0 +1,13 @@
+from coldtype import *
+
+@renderable()
+def mirror(r):
+    return (StSt("C", Font.ColdObvi(), 300)
+        .align(r.inset(100).take(0.5, "NW"))
+        .mirrorx(r.pc)
+        .mirrory(r.pc)
+        .layer(
+            lambda p: p.rotate(45, point=r.pc)
+                .f(hsl(0.9, 0.8)),
+            lambda p: p.f(hsl(0.65, 0.8))
+        ))
