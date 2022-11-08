b5c82a41678cad1088b8536df736a3bbc3f7455f
Rob Stenson <rob.stenson@gmail.com>
2022-11-08 10:01:06 -0800

    outline in main truchet case

--- examples/animations/truchet.py
+++ examples/animations/truchet.py
@@ -8,28 +8,38 @@ rs2 = random_series()
 tr = Rect(100)
 tn = 8
 
-at = AsciiTimeline(10, 30, """
-            <
-[0 ]    [0 ]
+at = AsciiTimeline(8, 30, """
+                            <
+[0 ]        [0 ]  
+[1  ]          [1  ]
+[2 ]              [2 ]  
 """)
 
-@animation((tr.w*tn, tr.w*tn), tl=at, bg=1)
+eases = ["beo", "eeo", "ceo"]
+colors = [hsl(0.17, 0.8), hsl(0.6, 0.8), hsl(0.95, 0.8)]
+
+@animation((tr.w*tn, tr.w*tn), tl=at, bg=0)
 def truchet1(f):
     def rotate(i, p):
         #row, col = i//tn, i%tn
         (p.rotate(90*int(rs[i]))
-            .rotate(f.t.ki(0).ec("eeio", rng=(0, 90)))
-            .f(hsl(rs2[i]))
-            .blendmode(BlendMode.Cycle(11)))
+            .rotate(f.t.ki(i%3).ec(eases[i%3], rng=(0, 90))))
 
     return (P(tr)
-        .difference(P()
+        .intersection(P()
             .append(P().oval(tr).t(tr.w/2))
-            .append(P().oval(tr).t(-tr.w/2)))
-        .f(0)
+            .append(P().oval(tr).t(-tr.w/2))
+            .outline(1)
+            )
+        .f(1)
         .data(frame=tr)
         .layer(tn)
         .spread()
         .layer(tn)
         .stack()
         .mapv(rotate))
+
+def release(passes):
+    from coldtype.renderable.animation import gifski
+    gifski(truchet1, passes)
+    print("/release")
