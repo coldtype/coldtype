c79fb853e7b4ceb185a6dda4517ccfc4b8fdb702
Rob Stenson <rob.stenson@gmail.com>
2022-11-08 10:18:23 -0800

    simpler ec example

---
+++ examples/animations/ec.py
@@ -0,0 +1,17 @@
+from coldtype import *
+
+at = AsciiTimeline(2, """
+                                            <
+[A    ]   [A  ]     [A        ]     [A  ]
+            [B              ]
+""")
+
+@animation((540, 540), tl=at)
+def scratch(f):
+    return (StSt("I", Font.MuSan(), 300
+        , wdth=f.t.ki("B").e("eeio")
+        )
+        .align(f.a.r, tv=0)
+        .unframe()
+        .rotate(f.t.ki("A").ec("eeio", rng=(0, 90)))
+        .f(0))
