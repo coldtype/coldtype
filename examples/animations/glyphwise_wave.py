e9034e8936552834d2c6b1e78eeba0c1c5e5cd06
Rob Stenson <rob.stenson@gmail.com>
2022-12-08 09:35:13 -0800

    move the varwaves

--- examples/animations/glyphwise_wave.py
+++ examples/animations/glyphwise_wave.py
@@ -2,11 +2,13 @@ from coldtype import *
 
 @animation((1080, 1080/2), timeline=30)
 def cilati_wave(f:Frame):
-    return (Glyphwise("Cilati", lambda g:
-        Style("SwearCilatiVariable", 340,
+    return (Glyphwise("COLD\nTYPE".upper(), lambda g:
+        Style(Font.MuSan(), 270,
             #show_frames=1,
             #no_shapes=1,
-            opsz=(fa:=f.adj(g.i*20)).e(ease:="qeio", 1),
+            wdth=(fa:=f.adj(g.i*20)).e(ease:="qeio", 1),
             wght=fa.e(ease, 1)))
+        .xalign(f.a.r)
+        .lead(50)
         .f(0)
         .align(f.a.r, tx=0))
--- examples/animations/glyphwise_wave2.py
+++ examples/animations/glyphwise_wave2.py
@@ -2,11 +2,11 @@ from coldtype import *
 
 @animation((1080, 1080/2), timeline=50)
 def fatface_wave(f):
-    return (Glyphwise("WAVEFORM", lambda g:
-        (Style("OhnoFatfaceVariable",
+    return (Glyphwise("COLD", lambda g:
+        (Style(Font.MuSan(),
             100+(fa:=f.adj(g.i*4)).e(ease:="seio", 1)*150,
             wdth=fa.e(ease, 1),
-            opsz=fa.e(ease, 1),
+            wght=fa.e(ease, 1),
             rotate=-15+30*fa.e(ease, 1))))
         .align(f.a.r, h=200)
         .f(0))
