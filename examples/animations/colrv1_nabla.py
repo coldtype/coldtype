b9feff6b0b0b5881b47459e031037aecff03936b
Rob Stenson <rob.stenson@gmail.com>
2023-01-14 15:54:14 -0800

    fix nabla example

--- examples/animations/colrv1_nabla.py
+++ examples/animations/colrv1_nabla.py
@@ -3,7 +3,7 @@ from coldtype import *
 @animation((1080, 540), tl=30, bg=hsl(0.7, 0.4, 0.4))
 def nabla1(f):
     return (Glyphwise("COLRv1", lambda x:
-        Style("Nabla", 300
+        Style(Font.GoogleFont("Nabla"), 300
             , EDPT=f.adj(x.i*20).e("eei", r=(0.1, 1))))
         .align(f.a.r, tx=0, ty=0)
         .translate(0, -30))
