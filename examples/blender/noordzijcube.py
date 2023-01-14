a256c004c2b139f78a06039af32308af6c5f4dba
Rob Stenson <rob.stenson@gmail.com>
2023-01-13 18:00:27 -0800

    minor blender example tweaks

--- examples/blender/noordzijcube.py
+++ examples/blender/noordzijcube.py
@@ -3,34 +3,34 @@ from coldtype.blender import *
 
 fnt = Font.Find("ObviouslyV")
 
-d = 5
+d = 4
 
 @b3d_animation(timeline=120, center=(0, 1))
 def cube(f):
     def entry(g, x, y, z):
-        return (StSt("B", fnt,
-            f.e("seio", 1, rng=(50, 550)),
-            ro=1,
-            wght=x/(d-1),
-            wdth=(y/(d-1)),
-            slnt=(z/(d-1)))
+        return (StSt("A", fnt
+            , fontSize=f.e("seio", 1, rng=(50, 550))
+            , ro=1
+            , slnt=x/(d-1)
+            , wdth=(y/(d-1))
+            , wght=(z/(d-1)))
             .pen()
-            .f(None).s(0).sw(1)
-            .f(1)
+            .fssw(1, 0, 1)
             .align(g)
-            .ch(b3d_pre(lambda p: p.fssw(1, -1, 0)))
             .ch(b3d(lambda bp: (bp
                 .extrude(0.1+x/(d-1)*0.1)
                 .rotate(90)
                 .locate(0, 10.8*(z/d), 0)))))
 
     gs = f.a.r.grid(d, d)
-    out = PS()
+    out = P()
     gi = 0
     gs = gs * d
+    
     for z in range(0, d):
         for y in range(0, d):
             for x in range(0, d):
                 out += entry(gs[gi], x, y, z)
                 gi += 1
+    
     return out
--- examples/blender/varfont2.py
+++ examples/blender/varfont2.py
@@ -2,16 +2,15 @@ from coldtype import *
 from coldtype.blender import *
 
 @b3d_runnable(playback=B3DPlayback.KeepPlaying)
-def prerun(bw):
-    pass
+def prerun(bw:BpyWorld):
+    bw.deletePrevious(materials=False)
 
 @b3d_animation(timeline=60, center=(0, 1), upright=1)
 def varfont2(f):
     return (P(
         Glyphwise("COLD\nTYPE", lambda g:
-            Style(Font.ColdtypeObviously(), 375,
-                wdth=f.adj(-g.i*5)
-                    .e("seio", 1, rng=(0.98, 0))))
+            Style(Font.ColdtypeObviously(), 375
+                , wdth=f.adj(-g.i*5).e("seio", 1, rng=(0.98, 0))))
             .xalign(f.a.r)
             .track(50, v=1)
             .align(f.a.r.drop(100, "S"))
