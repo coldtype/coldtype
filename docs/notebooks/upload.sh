e59c1770b8e3429e5d7b45e5f524a011f935a129
Rob Stenson <rob.stenson@gmail.com>
2023-03-23 14:46:55 -0700

    minor changes

--- docs/notebooks/upload.sh
+++
@@ -1,2 +0,0 @@
-aws s3 --region us-east-1 sync --cache-control no-cache --exclude "*" --include "*.html" docs/notebook/site s3://coldtype.goodhertz.com
-aws s3 --region us-east-1 sync docs/notebook/site s3://coldtype.goodhertz.com
