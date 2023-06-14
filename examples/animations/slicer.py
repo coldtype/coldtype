d296a9d0d02c028ecb408c8ba59ea40f53d6a597
Rob Stenson <rob.stenson@gmail.com>
2023-06-14 07:37:42 -0700

    simpler line

--- examples/animations/slicer.py
+++ examples/animations/slicer.py
@@ -5,8 +5,7 @@ rs = random_series(-(r:=50), r, seed=0)
 
 @animation((1080, 1080), bg=0, tl=120)
 def slicer(f):
-    s = Scaffold(f.a.r.inset(-500, 0))
-    s.grid(21, 1)
+    s = Scaffold(f.a.r.inset(-500, 0)).grid(21, 1)
     
     txt = (StSt("COLD\nTYPE", Font.ColdObvi(), f.e(r=(180, 440)), wght=1, leading=50)
         .xalign(f.a.r)
