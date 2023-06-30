3e64b52dac4b47f5b822d431952e81f3df628d1c
Rob Stenson <rob.stenson@gmail.com>
2023-06-30 08:09:16 -0700

    try/except for b3denv since that assumes blender is installed

--- coldtype/renderer/config.py
+++ coldtype/renderer/config.py
@@ -13,9 +13,12 @@ def true_false_or_none(x):
     
 
 def default_blender_app_path():
-    from b3denv import get_vars
-    b3d_vars = get_vars(None)
-    return Path(b3d_vars["blender"])
+    try:
+        from b3denv import get_vars
+        b3d_vars = get_vars(None)
+        return Path(b3d_vars["blender"])
+    except:
+        return None
 
 
 def mod_blender_app_path(bap):
