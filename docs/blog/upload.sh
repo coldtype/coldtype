adc9f2b5dc5c367efacb7b56a90dca2fcca77397
Rob Stenson <rob.stenson@gmail.com>
2024-07-01 09:39:20 -0700

    delete old blog

--- docs/blog/upload.sh
+++
@@ -1,2 +0,0 @@
-aws s3 --region us-west-1 sync --cache-control no-cache --exclude "*" --include "*.html" --include "*.xml" site s3://blog.coldtype.xyz --profile personal
-aws s3 --region us-west-1 sync site s3://blog.coldtype.xyz --profile personal
