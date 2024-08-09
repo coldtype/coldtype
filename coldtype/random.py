30174e0a6a12184efb269ee74871f432f9f1aca5
Rob Stenson <rob.stenson@gmail.com>
2024-08-09 15:28:32 -0700

    random_series with ease

--- coldtype/random.py
+++ coldtype/random.py
@@ -1,9 +1,15 @@
 from random import Random
 
-def random_series(start=0, end=1, seed=0, count=5000):
+def random_series(start=0, end=1, seed=0, count=5000, ease=None):
+    from coldtype.timing.easing import ez
+
     rnd = Random()
     rnd.seed(seed)
     rnds = []
-    for x in range(count):
-        rnds.append(start+rnd.random()*(end-start))
+    for _ in range(count):
+        e = rnd.random()
+        if ease:
+            rnds.append(ez(e, ease, rng=(start, end)))
+        else:
+            rnds.append(start+rnd.random()*(end-start))
     return rnds
