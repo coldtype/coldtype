b5e174ac3f6e3ab80f7ddf96b922afa33faf95e4
Rob Stenson <rob.stenson@gmail.com>
2024-10-07 16:15:08 -0700

    bump to 0.10.22

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ basic_deps = [
 
 setuptools.setup(
     name="coldtype",
-    version="0.10.21",
+    version="0.10.22",
     author="Rob Stenson / Goodhertz",
     author_email="rob@goodhertz.com",
     description="Functions for manual vectorized typesetting",
