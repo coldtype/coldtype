8f6de6af3279d3d2606f1967033dd12103fa3803
Rob Stenson <rob.stenson@gmail.com>
2024-12-12 19:47:54 -0800

    dont use ditto for transparency blocks

--- examples/transparency.py
+++ examples/transparency.py
@@ -2,12 +2,20 @@ from coldtype import *
 
 f = bw(0.9)
 
-@renderable((d:=30, d), bg=1, render_bg=1)
+@renderable(30, bg=1, render_bg=1)
 def blocks(r):
-    g = Grid(r, "a a", "a a", "a b / c d")
-    return (P(g["a"]).f(f), P(g["d"]).f(f))
+    s = Scaffold(r).numeric_grid(2, 2)
+    return [P(s["0|1"]).f(f), P(s["1|0"]).f(f)]
 
-def release(passes):
-    import subprocess
-    subprocess.run(["ditto", passes[0].output_path, "coldtype/demo/"])
-    print("ditto'd")
+@renderable(1024, bg=-1)
+def appicon(r):
+    return (StSt("C", Font.ColdObvi(), 1280)
+        .align(r))
+
+def release(_):
+    from shutil import copy2
+    blocks_path = blocks.render_to_disk(render_bg=1)[0]
+    copy2(blocks_path, Path("coldtype/demo") / blocks_path.name)
+
+    icon_path = appicon.render_to_disk(render_bg=0)[0]
+    copy2(icon_path, Path("coldtype/demo") / icon_path.name)
