793cfb180603bda520d9c381c593b54006fa1ff1
Rob Stenson <rob.stenson@gmail.com>
2025-01-12 13:12:07 -0800

    rewrite penangle example with samples and without freeze

--- examples/animations/penangle.py
+++ examples/animations/penangle.py
@@ -1,30 +1,25 @@
 from coldtype import *
 from coldtype.raster import phototype
-from coldtype.fx.skia import freeze
 
-@animation(bg=0, tl=240)
+r = Rect(1080)
+ri = r.inset(160)
+p = P().m(ri.psw).ioc(ri.pne, 50).fssw(-1, 1, 10)
+
+def angler(e):
+    return ez(e, "seio", 1, rng=(-90, -110))
+
+def hatch(pt, a=-45, w=100, h=5):
+    return (P()
+        .rect(Rect.FromCenter(pt, w, h))
+        .rotate(a)
+        .f(1))
+
+samples = p.samples(7)
+curve = p.enumerate(samples, lambda x: hatch(x.el.pt, angler(x.e))).ch(phototype(r, 5, 90, 13))
+
+@animation(bg=0, tl=Timeline(240, 48))
 def scratch(f):
-    ri = f.a.r.inset(160)
-    dt = P().m(ri.psw).ioc(ri.pne, 50).fssw(-1, 1, 10)
-
-    def pt(t, a=-45, w=100, h=5):
-        return (P()
-            .rect(Rect.FromCenter(dt.point_t(t)[0], w, h))
-            .rotate(a)
-            .f(1))
-    
-    def angler(e):
-        return ez(e, "seio", 1, rng=(-90, -110))
-    
-    def strokes():
-        return (P().enumerate(range(0, 250), lambda x: pt(x.e, angler(x.e)))
-            .ch(phototype(f.a.r, 5, 90, 13)))
-    
-    form = freeze(1, 1, strokes, [angler])
-
     return (P(
-        #dt,
-        form,
-        pt(f.e("seio", 1), angler(f.e("seio", 1)), 102, 20)
-            .ch(phototype(f.a.r, 3, 120, 30, hsl(0.07, 0.90, 0.50))),
-    ))
+        curve,
+        hatch(p.point_t(f.e("seio", 1))[0], angler(f.e("seio", 1)), 102, 20)
+            .ch(phototype(f.a.r, 3, 120, 30, hsl(0.07, 0.90, 0.50)))))
