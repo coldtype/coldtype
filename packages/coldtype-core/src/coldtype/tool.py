883840f1a060de533dc1a8717b60a262b5167c94
Rob Stenson <rob@goodhertz.com>
2026-06-02 11:54:29 -0700

    change syntax fo ƒ and §

--- packages/coldtype-core/src/coldtype/tool.py
+++ packages/coldtype-core/src/coldtype/tool.py
@@ -119,13 +119,13 @@ class Tool:
                 else:
                     if isinstance(v, str):
                         if k == "font":
-                            variated = v.split("¶")
+                            variated = v.split("§")
                             if len(variated) > 1:
                                 out["fontVariations"] = eval(f"dict({variated[1]})")
                             else:
                                 out["fontVariations"] = {}
                             
-                            filtered = variated[0].split("§")
+                            filtered = variated[0].split("ƒ")
                             if len(filtered) > 1:
                                 out["fontCond"] = filtered[-1]
                             else:
